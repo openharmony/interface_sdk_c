f32536a768065b7e7ef908d49d320b3078055470
xuerunlong <xuerunlong1@huawei.com>
2024-05-13 15:31:31 +0800

    fixed 534296c from https://gitee.com/xue-runlong/interface_sdk_c/pulls/679 ndk资料整改
    Signed-off-by: xuerunlong <xuerunlong1@huawei.com>

--- filemanagement/fileio/include/oh_fileio.h
+++ filemanagement/fileio/include/oh_fileio.h
@@ -63,6 +63,9 @@ typedef enum FileIO_FileLocation {
  * @param uriLength Input the length of the uri.
  * @param location Output the result of file location.
  * @return Return the status code of the execution.
+ *         {@link PARAMETER_ERROR} 401 - Invalid input parameter, pointer is null.
+ *         {@link E_NONET} 13900002 - No such file or directory.
+ *         {@link E_NOMEM} 13900011 - Failed to apply for memory.
  * @since 12
  */
 FileManagement_ErrCode OH_FileIO_GetFileLocation(char *uri, int uriLength,
