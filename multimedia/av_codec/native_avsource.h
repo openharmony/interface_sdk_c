43aa65e082ec2e7b66a5bc0ad038605d766c9927
wangyan <wangyan431@huawei.com>
2024-05-17 14:12:38 +0800

    整改CAPI接口返回描述
    Signed-off-by: wangyan <wangyan431@huawei.com>

--- multimedia/av_codec/native_avsource.h
+++ multimedia/av_codec/native_avsource.h
@@ -32,7 +32,8 @@ typedef struct OH_AVSource OH_AVSource;
  * @syscap SystemCapability.Multimedia.Media.Spliter
  * @param dataSource An Struct for a remote media resource.
  * @return Returns a pointer to an OH_AVSource instance if the execution is successful, otherwise returns nullptr.
- * Possible failure causes: 1. dataSource is nullptr. 2. dataSource->size == 0. 3. create AVSource failed.
+ * Possible failure causes: 1. dataSource is nullptr. 2. dataSource->size == 0. 3. set data source failed.
+ * 4. out of memory. 5. demuxer engine is nullptr.
  * @since 12
 */
 OH_AVSource *OH_AVSource_CreateWithDataSource(OH_AVDataSource *dataSource);
