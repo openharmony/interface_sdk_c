1035e1f41e1421d4715934ef041bfb7806a3c585
jiangchunli <jiangchunli1@huawei.com>
2024-05-22 18:10:57 +0800

    Change audio decryption interface from audio_decoder to audio_codec.
    Signed-off-by: jiangchunli <jiangchunli1@huawei.com>

--- multimedia/av_codec/native_avcodec_audiocodec.h
+++ multimedia/av_codec/native_avcodec_audiocodec.h
@@ -25,6 +25,13 @@ extern "C" {
 #endif
 
 /**
+ * @brief MediaKeySession field.
+ * @since 12
+ * @version 1.0
+ */
+typedef struct MediaKeySession MediaKeySession;
+
+/**
  * @brief Create an audio encoder or decoder instance from the mime type, which is recommended in most cases.
  * @syscap SystemCapability.Multimedia.Media.AudioCodec
  * @param mime mime type description string, refer to {@link AVCODEC_MIME_TYPE}
@@ -201,6 +208,19 @@ OH_AVErrCode OH_AudioCodec_FreeOutputBuffer(OH_AVCodec *codec, uint32_t index);
  */
 OH_AVErrCode OH_AudioCodec_IsValid(OH_AVCodec *codec, bool *isValid);
 
+/**
+ * @brief Set decryption info.
+ * @syscap SystemCapability.Multimedia.Media.AudioCodec
+ * @param codec Pointer to an OH_AVCodec instance
+ * @param mediaKeySession A media key session instance with decryption function.
+ * @param secureAudio Require secure decoder or not.
+ * @return Returns AV_ERR_OK if the execution is successful,
+ * otherwise returns a specific error code, refer to {@link OH_AVErrCode}
+ * @since 12
+ * @version 1.0
+*/
+OH_AVErrCode OH_AudioCodec_SetDecryptionConfig(OH_AVCodec *codec, MediaKeySession *mediaKeySession,
+    bool secureAudio);
 #ifdef __cplusplus
 }
 #endif
