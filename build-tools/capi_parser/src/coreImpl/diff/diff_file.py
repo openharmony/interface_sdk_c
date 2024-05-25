5b9d17d30cd65399e56437fb7283f9212a020602
zhangwuf <zhangwu47@huawei.com>
2024-05-25 11:46:11 +0800

    调试pandas版本
    Signed-off-by: zhangwuf <zhangwu47@huawei.com>

--- build-tools/capi_parser/src/coreImpl/diff/diff_file.py
+++ build-tools/capi_parser/src/coreImpl/diff/diff_file.py
@@ -92,6 +92,8 @@ def get_api_change_obj(api_data):
             compatible_dict['不兼容'] = 1
         if element.api_modification_type in modification_type_dict:
             modification_type_dict[element.api_modification_type] = 1
+    if 1 == modification_type_dict.get('API修改(原型修改)') or 1 == modification_type_dict.get('API修改(约束变化)'):
+        modification_type_dict['API修改'] = 1
     compatible_str = change_to_json(compatible_dict)
     modification_type_str = change_to_json(modification_type_dict)
     change_data_obj.set_compatible(compatible_str)
