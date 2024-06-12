f1151e29d8cd6cd59bcb48e8839afb59d235c107
huqingyun <huqingyun1@huawei.com>
2024-06-12 07:14:19 +0000

    5.0Beta1 回退OH_ArkUI_PointerEvent_SetStopPropagation接口提交
    Signed-off-by: huqingyun <huqingyun1@huawei.com>

--- arkui/ace_engine/native/ui_input_event.h
+++ arkui/ace_engine/native/ui_input_event.h
@@ -687,18 +687,6 @@ int32_t OH_ArkUI_MouseEvent_GetMouseButton(const ArkUI_UIInputEvent* event);
  */
 int32_t OH_ArkUI_MouseEvent_GetMouseAction(const ArkUI_UIInputEvent* event);
 
-/**
- * @brief Sets whether to prevent event bubbling.
- *
- * @param event Indicates the pointer to the current UI input event.
- * @param stopPropagation Indicates whether the event is prevented from bubbling.
- * @return Returns the status code of the execution. If 0 is returned, the setting is successful.
- *         If 401 is returned, the execution fails.
- *         The possible cause of the failure is that the event parameter is abnormal, such as a null pointer.
- * @since 12
- */
-int32_t OH_ArkUI_PointerEvent_SetStopPropagation(const ArkUI_UIInputEvent* event, bool stopPropagation);
-
 #ifdef __cplusplus
 };
 #endif
