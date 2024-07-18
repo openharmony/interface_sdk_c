d035b7469c24c9849dc0ad3d02a84d7383a65737
邹林肯 <735001321@qq.com>
2024-07-18 07:05:56 +0000

    update arkui/ace_engine/native/native_node.h.
    Signed-off-by: 邹林肯 <735001321@qq.com>

--- arkui/ace_engine/native/native_node.h
+++ arkui/ace_engine/native/native_node.h
@@ -623,11 +623,11 @@ typedef enum {
      * @brief Defines the custom shadow effect. This attribute can be set, reset, and obtained as required through APIs.
      *
      * Format of the {@link ArkUI_AttributeItem} parameter for setting the attribute:\n
-     * .value[0]?.f32: blur radius of the shadow, in px.\n
+     * .value[0]?.f32: blur radius of the shadow, in vp.\n
      * .value[1]?.i32: whether to enable the coloring strategy. The value <b>1</b> means to enable the coloring
      * strategy, and <b>0</b> (default value) means the opposite.\n
-     * .value[2]?.f32: offset of the shadow along the x-axis, in vp.\n
-     * .value[3]?.f32: offset of the shadow along the y-axis, in vp.\n
+     * .value[2]?.f32: offset of the shadow along the x-axis, in px.\n
+     * .value[3]?.f32: offset of the shadow along the y-axis, in px.\n
      * .value[4]?.i32: shadow type {@link ArkUI_ShadowType}. The default value is <b>ARKUI_SHADOW_TYPE_COLOR</b>.\n
      * .value[5]?.u32: shadow color, in 0xARGB format. For example, 0xFFFF0000 indicates red.\n
      * .value[6]?.u32: whether to fill the shadow. The value <b>1</b> means to fill the shadow, and <b>0</b>
@@ -635,10 +635,10 @@ typedef enum {
      *
      * \n
      * Format of the return value {@link ArkUI_AttributeItem}:\n
-     * .value[0].f32: blur radius of the shadow, in px.\n
+     * .value[0].f32: blur radius of the shadow, in vp.\n
      * .value[1].i32: whether to enable the coloring strategy. \n
-     * .value[2].f32: offset of the shadow along the x-axis, in vp.\n
-     * .value[3].f32: offset of the shadow along the y-axis, in vp.\n
+     * .value[2].f32: offset of the shadow along the x-axis, in px.\n
+     * .value[3].f32: offset of the shadow along the y-axis, in px.\n
      * .value[4].i32: shadow type {@link ArkUI_ShadowType}. The default value is <b>ARKUI_SHADOW_TYPE_COLOR</b>.\n
      * .value[5].u32: shadow color, in 0xARGB format. For example, 0xFFFF0000 indicates red.\n
      * .value[6].u32: whether to fill the shadow. The value <b>1</b> means to fill the shadow, and <b>0</b>
