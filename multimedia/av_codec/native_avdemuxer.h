421cd3208f03130bd3a7e61075e2f1d414611b7e
姜春丽 <jiangchunli1@huawei.com>
2024-07-22 07:45:25 +0000

    update multimedia/av_codec/native_avdemuxer.h.
    Signed-off-by: 姜春丽 <jiangchunli1@huawei.com>

--- multimedia/av_codec/native_avdemuxer.h
+++ multimedia/av_codec/native_avdemuxer.h
@@ -41,7 +41,6 @@ extern "C" {
  * @since 10
  */
 typedef struct OH_AVDemuxer OH_AVDemuxer;
-
 /**
  * @brief Forward declaration of DRM_MediaKeySystemInfo.
  *
@@ -49,11 +48,6 @@ typedef struct OH_AVDemuxer OH_AVDemuxer;
  */
 typedef struct DRM_MediaKeySystemInfo DRM_MediaKeySystemInfo;
 
-/**
- * @brief After this callback is set, it will be invoked when DRM information is updated.
- * @param mediaKeySystemInfo DRM information.
- * @since 11
- */
 typedef void (*DRM_MediaKeySystemInfoCallback)(DRM_MediaKeySystemInfo* mediaKeySystemInfo);
 
 /**
