953ac3abaec34eb7d8c334c61bc740c473cc2f07
yuanjinghua <yuanjinghua1@huawei.com>
2024-08-15 15:46:11 +0800

    Add interfaces for NDK AVPlayer to implement OnError and OnInfo
    Signed-off-by: yuanjinghua <yuanjinghua1@huawei.com>
    Change-Id: Ie5f7ef84100daca3e0b3b1db502947272e42236d

--- multimedia/player_framework/avplayer.h
+++ multimedia/player_framework/avplayer.h
@@ -462,6 +462,8 @@ OH_AVErrCode OH_AVPlayer_SetLooping(OH_AVPlayer *player, bool loop);
  *         {@link AV_ERR_INVALID_VAL} if input player is nullptr, callback.onInfo or callback.onError is null,
  *         or player SetPlayerCallback failed.
  * @since 11
+ * @deprecated since 12
+ * @useinstead {@link OH_AVPlayer_SetPlayerOnInfoCallback} {@link OH_AVPlayer_SetPlayerOnErrorCallback}
  * @version 1.0
  */
 OH_AVErrCode OH_AVPlayer_SetPlayerCallback(OH_AVPlayer *player, AVPlayerCallback callback);
@@ -562,6 +564,32 @@ OH_AVErrCode OH_AVPlayer_GetMediaKeySystemInfo(OH_AVPlayer *player, DRM_MediaKey
 */
 OH_AVErrCode OH_AVPlayer_SetDecryptionConfig(OH_AVPlayer *player, MediaKeySession *mediaKeySession,
     bool secureVideoPath);
+
+/**
+ * @brief Method to set player information notify callback.
+ * @syscap SystemCapability.Multimedia.Media.AVPlayer
+ * @param player Pointer to an OH_AVPlayer instance.
+ * @param callback Pointer to callback function, nullptr indicates unregister callback.
+ * @param userData Pointer to user specific data.
+ * @return Function result code.
+ *         {@link AV_ERR_OK} if the execution is successful.
+ *         {@link AV_ERR_INVALID_VAL} if input player is null or player SetOnInfoCallback failed.
+ * @since 12
+ */
+OH_AVErrCode OH_AVPlayer_SetOnInfoCallback(OH_AVPlayer *player, OH_AVPlayerOnInfoCallback callback, void *userData);
+
+/**
+ * @brief Method to set player error callback.
+ * @syscap SystemCapability.Multimedia.Media.AVPlayer
+ * @param player Pointer to an OH_AVPlayer instance.
+ * @param callback Pointer to callback function, nullptr indicates unregister callback.
+ * @param userData Pointer to user specific data.
+ * @return Function result code.
+ *         {@link AV_ERR_OK} if the execution is successful.
+ *         {@link AV_ERR_INVALID_VAL} if input player is null or player SetOnErrorCallback failed.
+ * @since 12
+ */
+OH_AVErrCode OH_AVPlayer_SetOnErrorCallback(OH_AVPlayer *player, OH_AVPlayerOnErrorCallback callback, void *userData);
 
 #ifdef __cplusplus
 }
