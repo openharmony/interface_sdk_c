ae7bcb5488fc8b9530d6966a888127b87af8f440
陈乔异 <chenqiaoyi2@huawei.com>
2024-08-23 23:05:27 +0800

    删除不属于的合并
    Signed-off-by: 陈乔异 <chenqiaoyi2@huawei.com>

--- multimedia/image_framework/include/image/pixelmap_native.h
+++ multimedia/image_framework/include/image/pixelmap_native.h
@@ -774,38 +774,6 @@ Image_ErrorCode OH_PixelmapNative_CreateEmptyPixelmap(
     OH_Pixelmap_InitializationOptions *options, OH_PixelmapNative **pixelmap);
 
 /**
- * @brief Get metadata.
- *
- * @param pixelmap The Pixelmap pointer to be operated.
- * @param key Type of metadata.
- * @param value Value of metadata.
- * @return Returns {@link Image_ErrorCode} IMAGE_SUCCESS - if the operation is successful.
- * returns {@link Image_ErrorCode} IMAGE_BAD_PARAMETER - if invalid parameter, key and value are incorrect.
- * returns {@link Image_ErrorCode} IMAGE_DMA_NOT_EXIST - if DMA memory does not exist.
- * returns {@link Image_ErrorCode} IMAGE_COPY_FAILED - if memory copy failed.
- * @see OH_PixelmapNative
- * @since 12
- */
-Image_ErrorCode OH_PixelmapNative_GetMetadata(OH_PixelmapNative *pixelmap, OH_Pixelmap_HdrMetadataKey key,
-    OH_Pixelmap_HdrMetadataValue **value);
-
-/**
- * @brief Set metadata.
- *
- * @param pixelmap The Pixelmap pointer to be operated.
- * @param key Type of metadata.
- * @param value Value of metadata.
- * @return Returns {@link Image_ErrorCode} IMAGE_SUCCESS - if the operation is successful.
- * returns {@link Image_ErrorCode} IMAGE_BAD_PARAMETER - if invalid parameter, key and value are incorrect.
- * returns {@link Image_ErrorCode} IMAGE_DMA_NOT_EXIST - if DMA memory does not exist.
- * returns {@link Image_ErrorCode} IMAGE_COPY_FAILED - if memory copy failed.
- * @see OH_PixelmapNative
- * @since 12
- */
-Image_ErrorCode OH_PixelmapNative_SetMetadata(OH_PixelmapNative *pixelmap, OH_Pixelmap_HdrMetadataKey key,
-    OH_Pixelmap_HdrMetadataValue *value);
-
-/**
  * @brief Get the native buffer from the PixelMap.
  *
  * @param pixelmap The PixelMap to get the native buffer from.
