4380a0bd7c0740d0e01e9af4bf8ee3ef18f17f2e
caochuan@huawei.com <caochuan@huawei.com>
2024-09-20 15:17:05 +0800

    add media library 0826 api
    Signed-off-by: caochuan@huawei.com <caochuan@huawei.com>

--- multimedia/media_library/media_asset_manager_capi.h
+++ multimedia/media_library/media_asset_manager_capi.h
@@ -23,7 +23,7 @@
  */
 
 /**
- * @file media_asset_manager.h
+ * @file media_asset_manager_capi.h
  *
  * @brief Defines the media asset manager APIs.
  *
@@ -38,6 +38,8 @@
 
 #ifndef MULTIMEDIA_MEDIA_LIBRARY_NATIVE_MEDIA_ASSET_MANAGER_H
 #define MULTIMEDIA_MEDIA_LIBRARY_NATIVE_MEDIA_ASSET_MANAGER_H
+
+#include <stdbool.h>
 
 #include "media_asset_base_capi.h"
 
@@ -95,6 +97,30 @@ MediaLibrary_RequestId OH_MediaAssetManager_RequestVideoForPath(OH_MediaAssetMan
 bool OH_MediaAssetManager_CancelRequest(OH_MediaAssetManager* manager, const MediaLibrary_RequestId requestId);
 
 /**
+ * @brief Request moving photo object.
+ *
+ * @permission ohos.permission.READ_IMAGEVIDEO
+ * @param manager the pointer to {@link OH_MediaAssetManager} instance.
+ * @param mediaAsset the {@link OH_MediaAsset} instance of media file object to be requested.
+ * @param requestOptions the {@link MediaLibrary_RequestOptions} for image request strategy mode.
+ * @param requestId indicates the {@link MediaLibrary_RequestId} of the request, which is an output parameter.
+ * @param callback the {@link OH_MediaLibrary_OnMovingPhotoDataPrepared} that will be called
+ *                 when the requested source is prepared.
+ * @return {@link #MEDIA_LIBRARY_OK} if the method call succeeds.
+ *         {@link #MEDIA_LIBRARY_PARAMETER_ERROR} Parameter error. Possible causes:
+ *                                                1. Mandatory parameters are left unspecified.
+ *                                                2. Incorrect parameter types.
+ *                                                3. Parameter verification failed.
+ *         {@link #MEDIA_LIBRARY_OPERATION_NOT_SUPPORTED} if operation is not supported.
+ *         {@link #MEDIA_LIBRARY_PERMISSION_DENIED} if permission is denied.
+ *         {@link #MEDIA_LIBRARY_INTERNAL_SYSTEM_ERROR} if internal system error.
+ * @since 13
+*/
+MediaLibrary_ErrorCode OH_MediaAssetManager_RequestMovingPhoto(OH_MediaAssetManager* manager,
+    OH_MediaAsset* mediaAsset, MediaLibrary_RequestOptions requestOptions, MediaLibrary_RequestId* requestId,
+    OH_MediaLibrary_OnMovingPhotoDataPrepared callback);
+
+/**
  * @brief Request image resources based on different strategy modes.
  *
  * @permission ohos.permission.READ_IMAGEVIDEO
@@ -117,6 +143,19 @@ bool OH_MediaAssetManager_CancelRequest(OH_MediaAssetManager* manager, const Med
 MediaLibrary_ErrorCode OH_MediaAssetManager_RequestImage(OH_MediaAssetManager* manager, OH_MediaAsset* mediaAsset,
     MediaLibrary_RequestOptions requestOptions, MediaLibrary_RequestId* requestId,
     OH_MediaLibrary_OnImageDataPrepared callback);
+
+/**
+ * @brief Release the {@link OH_MediaAssetManager} instance.
+ *
+ * @param manager the {@link OH_MediaAssetManager} instance.
+ * @return {@link #MEDIA_LIBRARY_OK} if the method call succeeds.
+ *         {@link #MEDIA_LIBRARY_PARAMETER_ERROR} Parameter error. Possible causes:
+ *                                                1. Mandatory parameters are left unspecified.
+ *                                                2. Incorrect parameter types.
+ *                                                3. Parameter verification failed.
+ * @since 13
+*/
+MediaLibrary_ErrorCode OH_MediaAssetManager_Release(OH_MediaAssetManager* manager);
 
 #ifdef __cplusplus
 }
