82ffc8e4b3b140e9d9211978a3b8e1927e382808
lihui <lihui359@huawei.com>
2024-11-12 12:01:55 +0800

    cherry pick 625aed2 from https://gitee.com/li_hui180/interface_sdk_c/pulls/2121 cherry pick c1aa321 from https://gitee.com/li_hui180/interface_sdk_c/pulls/1709 api lint alarm fix
    Signed-off-by: lihui <lihui359@huawei.com>

--- graphic/graphic_2d/native_buffer/native_buffer.h
+++ graphic/graphic_2d/native_buffer/native_buffer.h
@@ -13,9 +13,6 @@
  * limitations under the License.
  */
 
-#ifndef NDK_INCLUDE_NATIVE_BUFFER_H_
-#define NDK_INCLUDE_NATIVE_BUFFER_H_
-
 /**
  * @addtogroup OH_NativeBuffer
  * @{
@@ -38,6 +35,9 @@
  * @since 9
  * @version 1.0
  */
+
+#ifndef NDK_INCLUDE_NATIVE_BUFFER_H_
+#define NDK_INCLUDE_NATIVE_BUFFER_H_
 
 #include <stdint.h>
 #include <native_window/external_window.h>
