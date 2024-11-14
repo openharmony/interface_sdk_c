45a8d4af97339dd670578b7bfd7b7bbf978f444b
冉召宇 <ranzhaoyu1@huawei.com>
2024-11-14 01:57:21 +0000

    update global/resource_management/include/rawfile/raw_dir.h.
    Signed-off-by: 冉召宇 <ranzhaoyu1@huawei.com>

--- global/resource_management/include/rawfile/raw_dir.h
+++ global/resource_management/include/rawfile/raw_dir.h
@@ -31,7 +31,7 @@
  * @brief Declares native functions related to raw file directories.
  *
  * For example, you can use the functions to traverse and close a raw file directory, and reset its index.
- * 
+ *
  * @syscap SystemCapability.Global.ResourceManager
  * @library librawfile.z.so
  * @kit LocalizationKit
