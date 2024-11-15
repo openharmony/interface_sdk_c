66503f84243972da50e11b115d69a1493b007ef0
liuzerun <liuzerun@huawei.com>
2024-11-15 03:07:26 +0000

    API 规范整改
    Signed-off-by: liuzerun <liuzerun@huawei.com>

--- filemanagement/fileio/include/error_code.h
+++ filemanagement/fileio/include/error_code.h
@@ -13,9 +13,6 @@
  * limitations under the License.
  */
 
-#ifndef FILE_MANAGEMENT_FILEIO_ERROR_CODE_H
-#define FILE_MANAGEMENT_FILEIO_ERROR_CODE_H
-
 /**
  * @addtogroup FileIO
  * @{
@@ -32,6 +29,9 @@
  * @syscap SystemCapability.FileManagement.File.FileIO
  * @since 12
  */
+
+#ifndef FILE_MANAGEMENT_FILEIO_ERROR_CODE_H
+#define FILE_MANAGEMENT_FILEIO_ERROR_CODE_H
 
 #ifdef __cplusplus
 extern "C" {
