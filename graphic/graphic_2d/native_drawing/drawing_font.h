7927feab5ace53e871d040f98999908225be47f5
J1ngxuanWei <wjx021002@gmail.com>
2024-12-10 10:53:30 +0800

    themefont
    Signed-off-by: J1ngxuanWei <wjx021002@gmail.com>

--- graphic/graphic_2d/native_drawing/drawing_font.h
+++ graphic/graphic_2d/native_drawing/drawing_font.h
@@ -552,6 +552,34 @@ OH_Drawing_ErrorCode OH_Drawing_FontGetPathForGlyph(const OH_Drawing_Font* font,
 OH_Drawing_ErrorCode OH_Drawing_FontGetTextPath(const OH_Drawing_Font* font, const void* text, size_t byteLength,
     OH_Drawing_TextEncoding encoding, float x, float y, OH_Drawing_Path* path);
 
+/**
+ * @brief Sets whether to follow the theme font. If the value is true, the theme font is used when typeface is not set.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param Font Indicates the pointer to an <b>OH_Drawing_Font</b> object.
+ * @param followed Indicates whether to follow the theme font.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if Font is nullptr.
+ * @since 16
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_FontSetThemeFontFollowed(OH_Drawing_Font* Font, bool followed);
+
+/**
+ * @brief Gets whether to follow the theme font.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param Font Indicates the pointer to an <b>OH_Drawing_Font</b> object.
+ * @param followed Indicates whether to follow the theme font.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if Font or followed is nullptr.
+ * @since 16
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_FontIsThemeFontFollowed(const OH_Drawing_Font* Font, bool* followed);
+
 #ifdef __cplusplus
 }
 #endif
