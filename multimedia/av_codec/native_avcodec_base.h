16ea931edbfe1f74b010a42aa433bdcbca6ead67
peng <yangpeng43@huawei.com>
2024-12-25 04:24:05 +0000

    add muxer set format
    Signed-off-by: peng <yangpeng43@huawei.com>

--- multimedia/av_codec/native_avcodec_base.h
+++ multimedia/av_codec/native_avcodec_base.h
@@ -967,6 +967,14 @@ extern const char *OH_MD_KEY_VIDEO_DECODER_OUTPUT_COLOR_SPACE;
 extern const char *OH_MD_KEY_VIDEO_DECODER_OUTPUT_ENABLE_VRR;
 
 /**
+ * @brief Key for creation timestamp of a media file, value type is string.
+ *
+ * @syscap SystemCapability.Multimedia.Media.CodecBase
+ * @since 14
+ */
+extern const char *OH_MD_KEY_CREATION_TIME;
+
+/**
  * @brief Media type.
  *
  * @syscap SystemCapability.Multimedia.Media.CodecBase
