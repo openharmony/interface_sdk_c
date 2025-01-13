e86c9283b3b9439f3350ed757059327d0e4f152c
lihui <lihui359@huawei.com>
2025-01-13 17:13:30 +0800

    deprecated GET_STRIDE/SET_STRIDE
    Signed-off-by: lihui <lihui359@huawei.com>

--- graphic/graphic_2d/native_window/external_window.h
+++ graphic/graphic_2d/native_window/external_window.h
@@ -142,28 +142,28 @@ typedef enum NativeWindowOperation {
      * set native window buffer stride,
      * variable parameter in function is
      * [in] int32_t stride.
-     * @deprecated since 14
+     * @deprecated since 16
      */
     SET_STRIDE,
     /**
      * get native window buffer stride,
      * variable parameter in function is
      * [out] int32_t *stride.
-     * @deprecated since 14
+     * @deprecated since 16
+     * @useinstead Use {@link OH_NativeWindow_GetBufferHandleFromNative} to get a {@link BufferHandleand} from a buffer
+     * and then retrieve the stride from the {@link BufferHandle}.
      */
     GET_STRIDE,
     /**
      * set native window buffer swap interval,
      * variable parameter in function is
      * [in] int32_t interval.
-     * @deprecated since 14
      */
     SET_SWAP_INTERVAL,
     /**
      * get native window buffer swap interval,
      * variable parameter in function is
      * [out] int32_t *interval.
-     * @deprecated since 14
      */
     GET_SWAP_INTERVAL,
     /**
