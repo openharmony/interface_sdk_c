539ccba61dc17df2b80e6bdf1541272d2cb81fc4
尹耀德 <yinyaode1@huawei.com>
2025-01-22 16:32:07 +0800

    asset add wrap
    Signed-off-by: 尹耀德 <yinyaode1@huawei.com>

--- security/asset/inc/asset_type.h
+++ security/asset/inc/asset_type.h
@@ -217,6 +217,12 @@ typedef enum {
      * @since 16
      */
     ASSET_TAG_GROUP_ID = ASSET_TYPE_BYTES | 0x48,
+    /**
+     * A tag whose value is a 32-bit unsigned integer indicating the type of Asset encapsulation.
+     *
+     * @since 16
+     */
+    ASSET_TAG_WRAP_TYPE = ASSET_TYPE_BYTES | 0x49,
 } Asset_Tag;
 
 /**
@@ -329,6 +335,22 @@ typedef enum {
      */
     ASSET_SYNC_TYPE_TRUSTED_ACCOUNT = 1 << 2,
 } Asset_SyncType;
+
+/**
+ * @brief An enum type indicates the type of Asset encapsulation.
+ *
+ * @since 16
+ */
+typedef enum {
+    /**
+     * An Asset with this attribute value is never allowed to be wrapped up.
+     */
+    ASSET_WRAP_TYPE_NEVER = 0,
+    /**
+     * An Asset with this attribute value can only be wrapped or unwrapped on devices logged in with trusted accounts.
+     */
+    ASSET_WRAP_TYPE_TRUSTED_ACCOUNT = 1,
+} Asset_WrapType;
 
 /**
  * @brief Enumerates the policies for resolving the conflict (for example, duplicate alias) occurred when
