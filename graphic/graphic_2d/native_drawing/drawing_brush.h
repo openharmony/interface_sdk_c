9b3c077eca32d97da9298131e044f59dc99e7d67
liuwei <liuwei793@h-partners.com>
2025-05-30 10:12:55 +0800

    drawing colorspace interface C upload
    Signed-off-by: liuwei <liuwei793@h-partners.com>

--- graphic/graphic_2d/native_drawing/drawing_brush.h
+++ graphic/graphic_2d/native_drawing/drawing_brush.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,11 +40,18 @@
 #ifndef C_INCLUDE_DRAWING_BRUSH_H
 #define C_INCLUDE_DRAWING_BRUSH_H
 
+#include "drawing_error_code.h"
 #include "drawing_types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @brief Defines a colorspace manager. Introduces the color space information defined by ColorManager.
+ * @since 20
+ */
+typedef struct OH_NativeColorSpaceManager OH_NativeColorSpaceManager;
 
 /**
  * @brief Creates an <b>OH_Drawing_Brush</b> object.
@@ -147,6 +154,83 @@ uint8_t OH_Drawing_BrushGetAlpha(const OH_Drawing_Brush* brush);
  * @version 1.0
  */
 void OH_Drawing_BrushSetAlpha(OH_Drawing_Brush* brush, uint8_t alpha);
+
+/**
+ * @brief Sets the color for a brush. The color will be used by the brush to fill in a shape.
+ * The color is an ARGB structure described by floating point numbers and interpreted as being in the colorSpaceManager.
+ * If colorSpaceManager is nullptr, then color is assumed to be in the sRGB color space.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param brush Indicates the pointer to an <b>OH_Drawing_Brush</b> object.
+ * @param a Indicates the alpha component of color, represented as a floating point number between 0 and 1.
+ * @param r Indicates the red component of color, represented as a floating point number between 0 and 1.
+ * @param g Indicates the green component of color, represented as a floating point number between 0 and 1.
+ * @param b Indicates the blue component of color, represented as a floating point number between 0 and 1.
+ * @param colorSpaceManager Indicates the pointer to an <b>OH_NativeColorSpaceManager</b> object.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if brush is nullptr.
+ * @since 20
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_BrushSetColor4f(OH_Drawing_Brush* brush, float a, float r, float g, float b,
+    OH_NativeColorSpaceManager* colorSpaceManager);
+
+/**
+ * @brief Obtains the alpha component of a brush.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param brush Indicates the pointer to an <b>OH_Drawing_Brush</b> object.
+ * @param a Indicates the alpha component of color.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if brush or a is nullptr.
+ * @since 20
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_BrushGetAlphaFloat(const OH_Drawing_Brush* brush, float* a);
+
+/**
+ * @brief Obtains the red component of a brush.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param brush Indicates the pointer to an <b>OH_Drawing_Brush</b> object.
+ * @param r Indicates the red component of color.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if brush or r is nullptr.
+ * @since 20
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_BrushGetRedFloat(const OH_Drawing_Brush* brush, float* r);
+
+/**
+ * @brief Obtains the green component of a brush.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param brush Indicates the pointer to an <b>OH_Drawing_Brush</b> object.
+ * @param g Indicates the green component of color.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if brush or g is nullptr.
+ * @since 20
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_BrushGetGreenFloat(const OH_Drawing_Brush* brush, float* g);
+
+/**
+ * @brief Obtains the blue component of a brush.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param brush Indicates the pointer to an <b>OH_Drawing_Brush</b> object.
+ * @param b Indicates the blue component of color.
+ * @return Returns the error code.
+ *         Returns {@link OH_DRAWING_SUCCESS} if the operation is successful.
+ *         Returns {@link OH_DRAWING_ERROR_INVALID_PARAMETER} if brush or b is nullptr.
+ * @since 20
+ * @version 1.0
+ */
+OH_Drawing_ErrorCode OH_Drawing_BrushGetBlueFloat(const OH_Drawing_Brush* brush, float* b);
 
 /**
  * @brief Sets the shaderEffect for a brush.
