2986318c8a869836cc7dfe6d2a3d738a99f2d2a5
colinhl <helin48@huawei.com>
2025-06-07 16:00:38 +0800

    fix
    Signed-off-by: colinhl <helin48@huawei.com>

--- multimedia/av_codec/native_avcodec_base.h
+++ multimedia/av_codec/native_avcodec_base.h
@@ -1071,7 +1071,7 @@ extern const char *OH_MD_KEY_MAX_BITRATE;
  * This is an optional key that applies only to video encoder, default is 0.
  * If enabled, the PTS information must be carried in each video frame and sent to the encoder.
  * It is used in configure.
- * 
+ *
  * @syscap SystemCapability.Multimedia.Media.CodecBase
  * @since 20
 */
