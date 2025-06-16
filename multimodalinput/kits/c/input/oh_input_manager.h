4acfea7e25f970b15a9e7793fc253a8b8bf87f7b
qianyong325 <qianyong15@h-partners.com>
2025-06-16 22:42:24 +0800

    checkcode
    Signed-off-by: qianyong325 <qianyong15@h-partners.com>

--- multimodalinput/kits/c/input/oh_input_manager.h
+++ multimodalinput/kits/c/input/oh_input_manager.h
@@ -1044,6 +1044,7 @@ Input_Result OH_Input_RequestInjection(Input_InjectAuthorizeCallback callback);
  *         {@link INPUT_SUCCESS} Success.\n
  *         {@link INPUT_PARAMETER_ERROR} The status is NULL\n
  *         {@link INPUT_SERVICE_EXCEPTION} Service error.\n
+ * @syscap SystemCapability.MultimodalInput.Input.Core
  * @since 20
  */
 
