d374cd210e437c6385201a09a2016299fcc4bc0a
jxw <jiexiwen@huawei.com>
2025-07-12 07:17:08 +0000

    update network/netmanager/include/net_connection.h.
    Signed-off-by: jxw <jiexiwen@huawei.com>

--- network/netmanager/include/net_connection.h
+++ network/netmanager/include/net_connection.h
@@ -395,8 +395,6 @@ int32_t OH_NetConn_QueryProbeResult(char *destination, int32_t duration, NetConn
  * @param traceRouteInfo Pointer to trace route result.
  * @return 0 - Success.
  *         201 - Missing permissions.
- *         401 - Parameter error.
- *         2100003 - Internal error.
  * @permission ohos.permission.INTERNET and ohos.permission.LOCATION and ohos.permission.ACCESS_TRACE_ROUTE_INFO
  * @since 20
  */
