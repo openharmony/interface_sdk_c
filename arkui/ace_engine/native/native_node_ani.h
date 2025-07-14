96e8adcb245aca34aff7ca4f7c9b69db023ff3b7
wangyang2022 <wangyang412@huawei.com>
2025-07-14 14:34:09 +0000

    C-API NAPI接口ANI适配 Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: I0fad9e5c35e2e35fee2b838d59655de29333f95f

---
+++ arkui/ace_engine/native/native_node_ani.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @addtogroup ArkUI_NativeModule
+ * @{
+ *
+ * @brief Provides UI capabilities of ArkUI on the native side, such as UI component creation and destruction,
+ * tree node operations, attribute setting, and event listening.
+ *
+ * @since 12
+ */
+
+/**
+ * @file native_node_ani.h
+ *
+ * @brief Declares APIs for converting <b>FrameNode</b> objects on the ArkTS side to <b>ArkUI_NodeHandle</b> objects on
+ * the native side.
+ *
+ * @library libace_ndk.z.so
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @kit ArkUI
+ * @since 20
+ */
+
+#ifndef ARKUI_NATIVE_NODE_ANI_H
+#define ARKUI_NATIVE_NODE_ANI_H
+
+#include "ani/ani.h"
+
+#include "native_type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Obtains a <b>FrameNode</b> object on the ArkTS side and maps it to an <b>ArkUI_NodeHandle</b> object on the
+ * native side.
+ *
+ * @param env Indicates the ANI environment pointer.
+ * @param frameNode Indicates the <b>FrameNode</b> object created on the ArkTS side.
+ * @param handle Indicates the pointer to the <b>ArkUI_NodeHandle</b> object.
+ * @return Returns the error code.
+ *         Returns {@link ARKUI_ERROR_CODE_NO_ERROR} if the operation is successful.
+ *         Returns {@link ARKUI_ERROR_CODE_PARAM_INVALID} if a parameter error occurs.
+ * @since 20
+ */
+int32_t OH_ArkUI_NativeModule_GetNodeHandleFromAniValue(ani_env* env, ani_object frameNode, ArkUI_NodeHandle* handle);
+
+/**
+ * @brief Obtains a <b>UIContext</b> object on the ArkTS side and maps it to an <b>ArkUI_ContextHandle</b> object on the
+ * native side.
+ *
+ * @param env ndicates the ANI environment pointer.
+ * @param context Indicates the <b>UIContext</b> object created on the ArkTS side.
+ * @param handle Indicates the pointer to the <b>ArkUI_ContextHandle</b> object.
+ * @return Returns the error code.
+ *         Returns {@link ARKUI_ERROR_CODE_NO_ERROR} if the operation is successful.
+ *         Returns {@link ARKUI_ERROR_CODE_PARAM_INVALID} if a parameter error occurs.
+ * @since 20
+ */
+int32_t OH_ArkUI_NativeModule_GetContextFromAniValue(ani_env* env, ani_object context, ArkUI_ContextHandle* handle);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // ARKUI_NATIVE_NODE_ANI_H
+/** @} */
