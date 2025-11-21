ec63c4ce54d822b5bf45ee0698c3acfa5e5c0d87
lcy_lovebug <lichengyi5@h-partners.com>
2025-11-21 17:41:51 +0800

    分组密钥新增接口
    Signed-off-by: lcy_lovebug <lichengyi5@h-partners.com>
    Change-Id: Ib3f84c9ba533aeb63409e27b52ffa24d9cfaf90e

--- security/huks/include/native_huks_type.h
+++ security/huks/include/native_huks_type.h
@@ -318,6 +318,12 @@ enum OH_Huks_AlgSuite {
      *  |   key_material_size_len     (4 Byte) |  key_material_size  |   key_mat_enc_length (4 Byte) | key_mat_enc_data
      */
     OH_HUKS_UNWRAP_SUITE_ECDH_AES_256_GCM_NOPADDING = 2,
+    /**
+     * @brief Use SM2 and then use SM4-ECB-NoPadding to encrypt the key
+     * 
+     * @since 23
+     */
+    OH_HUKS_UNWRAP_SUITE_SM2_SM4_ECB_NOPADDING = 5,
 };
 
 /**
@@ -789,6 +795,12 @@ enum OH_Huks_Tag {
      * @since 22
      */
     OH_HUKS_TAG_KEY_CLASS = OH_HUKS_TAG_TYPE_UINT | 522,
+    /**
+     * @brief The tag indicates a group of shared keys among applications with the same developer ID.
+     *
+     * @since 23
+     */
+    OH_HUKS_TAG_KEY_ACCESS_GROUP = OH_HUKS_TAG_TYPE_BYTES | 523,
     /**
      * 601 to 1000 are reserved for other tags.
      *
