c0a5c21fcd2e80a89b9037b95053d3923b339156
lishang21 <lishang21@h-partners.com>
2026-04-03 15:02:43 +0800

    update: 更新文件 oh_device_manager_err_code.h
    Signed-off-by: lishang21 <lishang21@h-partners.com>

--- distributedhardware/device_manager/include/oh_device_manager_err_code.h
+++ distributedhardware/device_manager/include/oh_device_manager_err_code.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @addtogroup DeviceManager
  * @{
@@ -22,13 +21,11 @@
  *
  * @since 20
  */
-
 /**
  * @file oh_device_manager_err_code.h
  *
  * @brief Declares the error codes of the distributed device management module.
- * File to include: <distributedhardware/device_manager/oh_device_manager_err_code.h>
- *
+ * 
  * @kit DistributedServiceKit
  * @library libdevicemanager_ndk.so
  * @syscap SystemCapability.DistributedHardware.DeviceManager
@@ -45,33 +42,46 @@ extern "C" {
 #endif
 
 /**
- * @brief Error code information of distributed device management module.
- *
+ * @brief Error codes of the distributed device management module.
+ * 
  * @since 20
  */
+
 typedef enum DeviceManager_ErrorCode {
     /**
-     * @error Operation success.
+     * @brief Operation success.
+     *
+     * @since 20
      */
     ERR_OK = 0,
     /**
-     * @error Permission verification failed.
+     * @brief Permission verification failed.
+     *
+     * @since 20
      */
     ERR_PERMISSION_ERROR = 201,
     /**
-     * @error Invalid parameter.
+     * @brief Invalid parameter.
+     *
+     * @since 20
      */
     ERR_INVALID_PARAMETER = 401,
     /**
-     * @error Function execution failed.
+     * @brief Function execution failed.
+     *
+     * @since 20
      */
     DM_ERR_FAILED = 11600101,
     /**
-     * @error Failed to obtain the device management service.
+     * @brief Failed to obtain the device management service.
+     *
+     * @since 20
      */
     DM_ERR_OBTAIN_SERVICE = 11600102,
     /**
-     * @error Failed to obtain the bundle name.
+     * @brief Failed to obtain the bundle name.
+     *
+     * @since 20
      */
     DM_ERR_OBTAIN_BUNDLE_NAME = 11600109,
 } DeviceManager_ErrorCode;
