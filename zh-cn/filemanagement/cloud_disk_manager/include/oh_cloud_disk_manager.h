80c4d37f0f9decaa3222b1b8e40c19e117509d9c
lishang21 <lishang21@h-partners.com>
2026-04-11 15:23:48 +0800

    update: 更新文件 oh_cloud_disk_manager.h
    Signed-off-by: lishang21 <lishang21@h-partners.com>

--- zh-cn/filemanagement/cloud_disk_manager/include/oh_cloud_disk_manager.h
+++ zh-cn/filemanagement/cloud_disk_manager/include/oh_cloud_disk_manager.h
@@ -25,7 +25,7 @@
  * @file oh_cloud_disk_manager.h
  *
  * @brief 云盘管理模块的接口定义。
- * 
+ *
  * @library libohclouddiskmanager.so
  * @kit CoreFileKit
  * @syscap SystemCapability.FileManagement.CloudDiskManager
@@ -47,7 +47,7 @@ extern "C" {
 
 /**
  * @brief 文件同步状态的枚举值。
- * 
+ *
  * @since 21
  *
  */
@@ -92,7 +92,7 @@ typedef enum CloudDisk_SyncState {
 
 /**
  * @brief 文件变更类型枚举值。
- * 
+ *
  * @since 21
  */
 typedef enum CloudDisk_OperationType {
@@ -136,7 +136,7 @@ typedef enum CloudDisk_OperationType {
 
 /**
  * @brief 文件同步失败原因的枚举值。
- * 
+ *
  * @since 21
  */
 typedef enum CloudDisk_ErrorReason {
@@ -174,7 +174,7 @@ typedef enum CloudDisk_ErrorReason {
 
 /**
  * @brief 文件路径信息。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_PathInfo {
@@ -208,7 +208,7 @@ typedef CloudDisk_PathInfo CloudDisk_SyncFolderPath;
 
 /**
  * @brief 文件的同步状态。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_FileSyncState {
@@ -228,7 +228,7 @@ typedef struct CloudDisk_FileSyncState {
 
 /**
  * @brief 定义了同步根路径下单个文件变更事件的数据结构。该结构包含有关文件变更的详细信息，包括唯一ID、父目录的唯一ID、相对路径、变更类型、文件大小和时间戳。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_ChangeData {
@@ -284,7 +284,7 @@ typedef struct CloudDisk_ChangeData {
 
 /**
  * @brief 查询同步根路径中文件变更的结果。该结构体包含同步根路径中文件的变更数据，包括下一个更新序列号、结尾标志以及变更数据项数组。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_ChangesResult {
@@ -316,7 +316,7 @@ typedef struct CloudDisk_ChangesResult {
 
 /**
  * @brief 同步操作中失败的文件列表信息。该结构包含文件路径信息以及失败的具体错误原因。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_FailedList {
@@ -336,7 +336,7 @@ typedef struct CloudDisk_FailedList {
 
 /**
  * @brief 表示一个文件同步操作的结果。该结构体包含文件的绝对路径、同步结果，以及同步状态或失败原因。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_ResultList {
@@ -368,7 +368,7 @@ typedef struct CloudDisk_ResultList {
 
 /**
  * @brief 同步根路径状态的枚举值。
- * 
+ *
  * @since 21
  */
 typedef enum CloudDisk_SyncFolderState {
@@ -388,7 +388,7 @@ typedef enum CloudDisk_SyncFolderState {
 
 /**
  * @brief 定义同步根路径的显示名称信息。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_DisplayNameInfo {
@@ -414,7 +414,7 @@ typedef struct CloudDisk_DisplayNameInfo {
 
 /**
  * @brief 同步根属性信息。
- * 
+ *
  * @since 21
  */
 typedef struct CloudDisk_SyncFolder {
@@ -440,7 +440,7 @@ typedef struct CloudDisk_SyncFolder {
 
 /**
  * @brief 应用注册一个回调函数，用于获取同步根路径下文件的变更。
- * 
+ *
  * @param syncFolderPath 表示同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @param callback 注册的回调函数。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
@@ -453,7 +453,7 @@ CloudDisk_ErrorCode OH_CloudDisk_RegisterSyncFolderChanges(const CloudDisk_SyncF
 
 /**
  * @brief 应用取消注册同步根路径下文件变更的回调。
- * 
+ *
  * @param syncFolderPath 表示同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
  * @since 21
@@ -462,7 +462,7 @@ CloudDisk_ErrorCode OH_CloudDisk_UnregisterSyncFolderChanges(const CloudDisk_Syn
 
 /**
  * @brief 获取同步根路径下的历史操作记录。
- * 
+ *
  * @param syncFolderPath 查询的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @param startUsn 查询起始的变更序列，范围：[0, 2^64 - 1]。
  * @param count 查询文件变更的数量，范围：[1, 100]。
@@ -477,7 +477,7 @@ CloudDisk_ErrorCode OH_CloudDisk_GetSyncFolderChanges(const CloudDisk_SyncFolder
 
 /**
  * @brief 应用设置同步根路径下文件的同步状态。
- * 
+ *
  * @param syncFolderPath 待设置的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @param fileSyncStates The array of {@link CloudDisk_FileSyncState} specifying the file paths and their target sync
  *     states.
@@ -495,7 +495,7 @@ CloudDisk_ErrorCode OH_CloudDisk_SetFileSyncStates(const CloudDisk_SyncFolderPat
 
 /**
  * @brief 应用查询同步根路径下文件同步状态。
- * 
+ *
  * @param syncFolderPath 待查询的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @param paths The array of file paths to query.
  * @param bufferLength 待查询同步状态的数组的长度，范围：[1, 100]。
@@ -512,7 +512,7 @@ CloudDisk_ErrorCode OH_CloudDisk_GetFileSyncStates(const CloudDisk_SyncFolderPat
 
 /**
  * @brief 应用注册同步根。
- * 
+ *
  * @param syncFolder 待注册的同步根路径，参考：{@link CloudDisk_SyncFolder}。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
  * @since 21
@@ -521,7 +521,7 @@ CloudDisk_ErrorCode OH_CloudDisk_RegisterSyncFolder(const CloudDisk_SyncFolder *
 
 /**
  * @brief 应用取消注册同步根。
- * 
+ *
  * @param syncFolderPath 需要取消注册的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
  * @since 21
@@ -530,7 +530,7 @@ CloudDisk_ErrorCode OH_CloudDisk_UnregisterSyncFolder(const CloudDisk_SyncFolder
 
 /**
  * @brief 应用激活同步根。
- * 
+ *
  * @param syncFolderPath 需要激活监听的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
  * @since 21
@@ -539,7 +539,7 @@ CloudDisk_ErrorCode OH_CloudDisk_ActiveSyncFolder(const CloudDisk_SyncFolderPath
 
 /**
  * @brief 应用取消激活同步根。
- * 
+ *
  * @param syncFolderPath 取消激活监听的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
  * @since 21
@@ -548,7 +548,7 @@ CloudDisk_ErrorCode OH_CloudDisk_DeactiveSyncFolder(const CloudDisk_SyncFolderPa
 
 /**
  * @brief 应用获取所有同步根。
- * 
+ *
  * @param syncFolders 输出参数。返回同步根路径数组{@link CloudDisk_SyncFolder}。
  * @param count 输出参数。当前网盘注册的所有同步根的数量。
  * @return 如果接口调用成功，返回{@link CLOUD_DISK_OK}；否则返回云盘管理模块的错误码{@link CloudDisk_ErrorCode}。
@@ -558,7 +558,7 @@ CloudDisk_ErrorCode OH_CloudDisk_GetSyncFolders(CloudDisk_SyncFolder **syncFolde
 
 /**
  * @brief 应用更新同步根别名。
- * 
+ *
  * @param syncFolderPath 待更新别名的同步根路径，参考：{@link CloudDisk_PathInfo}。
  * @param customAlias 用户定义的别名，不能包含字符：\\\/\*\?\<\>\|\:\"，以及不能以"."、".."和纯空格作为完整名称。
  * @param customAliasLength 用户定义的别名长度，范围：[0, 255]。
