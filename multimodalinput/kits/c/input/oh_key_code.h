338bfb04751124a92c2e6d897baf6ee6999b0e5f
guoxu1111 <guoxu34@h-partners.com>
2026-04-16 20:51:42 +0800

    update: 更新文件 oh_key_code.h adaptor Smart Control gesture keycode Co-Authored-By: Agent
    Signed-off-by: guoxu1111 <guoxu34@h-partners.com>

--- multimodalinput/kits/c/input/oh_key_code.h
+++ multimodalinput/kits/c/input/oh_key_code.h
@@ -1402,6 +1402,11 @@ typedef enum {
      */
     KEYCODE_DIV = 3220,
     /**
+     * XKEY
+     * @since 26.0.0
+     */
+    KEYCODE_XKEY = 3232,
+    /**
      * FINGERPRINT_SLIDE_UP
      * @since 26.0.0
      */
@@ -1410,12 +1415,7 @@ typedef enum {
      * FINGERPRINT_SLIDE_DOWN
      * @since 26.0.0
      */
-    KEYCODE_FINGERPRINT_SLIDE_DOWN = 3234
-    /**
-     * XKEY
-     * @since 26.0.0
-     */
-    KEYCODE_XKEY = 3232,
+    KEYCODE_FINGERPRINT_SLIDE_DOWN = 3234,
 } Input_KeyCode;
 
 #ifdef __cplusplus
