0541b80896d610d290b613a1b3adc9823b7de9f4
HarMonkey <yuanhaifeng6@huawei.com>
2026-05-11 10:39:48 +0800

    asset API一致性整改
    Signed-off-by: HarMonkey <yuanhaifeng6@huawei.com>

--- security/asset/inc/asset_api.h
+++ security/asset/inc/asset_api.h
@@ -35,24 +35,21 @@
  * @syscap SystemCapability.Security.Asset
  * @since 11
  */
-
 #ifndef ASSET_API_H
 #define ASSET_API_H
-
 #include <stdint.h>
 #include <stdlib.h>
-
 #include "asset_type.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /**
  * @brief Adds an asset. Permission ohos.permission.STORE_PERSISTENT_DATA is required when the Asset needs to be stored
- *     persistently by setting {@link ASSET_TAG_IS_PERSISTENT} tag.
+ * persistently by setting {@link ASSET_TAG_IS_PERSISTENT} tag.
  *
- * @param attributes Pointer to the attributes of the asset to add.
- * @param attributes Number of the attributes of the asset to add.
+ * @param attributes Attributes of the asset to add.
+ * @param attrCnt Number of the attributes of the asset to add.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_PERMISSION_DENIED} 201 - The caller doesn't have the permission.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
@@ -79,8 +76,8 @@ int32_t OH_Asset_Add(const Asset_Attr *attributes, uint32_t attrCnt);
 /**
  * @brief Removes one or more assets.
  *
- * @param query Pointer to the conditions for removing the assets.
- * @param queryCnt Number of conditions for removing the assets.
+ * @param query Attributes of the asset to remove.
+ * @param queryCnt Number of attributes.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
  *         1. Incorrect parameter types.
@@ -102,10 +99,10 @@ int32_t OH_Asset_Remove(const Asset_Attr *query, uint32_t queryCnt);
 /**
  * @brief Updates an asset.
  *
- * @param query Pointer to the conditions for updating the asset.
- * @param queryCnt Number of conditions for updating the asset.
- * @param attributes Pointer to the attributes of the asset to update.
- * @param attributes Number of the attributes of the asset to update.
+ * @param query Attributes of the asset to update.
+ * @param queryCnt Number of attributes to update.
+ * @param attributesToUpdate Pointer to the attributes of the asset to update.
+ * @param updateCnt Number of the attributes of the asset to update.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
  *         1. Mandatory parameters are left unspecified.
@@ -129,11 +126,12 @@ int32_t OH_Asset_Update(const Asset_Attr *query, uint32_t queryCnt,
     const Asset_Attr *attributesToUpdate, uint32_t updateCnt);
 
 /**
- * @brief Preprocesses data before querying the asset that can be accessed only after a successful user authentication.
+ * @brief Performs preprocessing for the asset query. This API is used when user authentication is required for the
+ * access to the asset.
  *
- * @param query Pointer to the search criteria of the asset.
- * @param queryCnt Number of the search criteria.
- * @param challenge Pointer to the challenge value to be used when <b>OH_Asset_Query</b> is called.
+ * @param query Attributes of the asset to query.
+ * @param queryCnt Number of attributes.
+ * @param challenge Challenge value, which is used when {@link OH_Asset_Query} is called.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
  *         1. Incorrect parameter types.
@@ -156,11 +154,11 @@ int32_t OH_Asset_Update(const Asset_Attr *query, uint32_t queryCnt,
 int32_t OH_Asset_PreQuery(const Asset_Attr *query, uint32_t queryCnt, Asset_Blob *challenge);
 
 /**
- * @brief Queries assets.
+ * @brief Queries one or more assets.
  *
- * @param query Pointer to the search criteria.
- * @param queryCnt Number of the search criteria.
- * @param resultSet Pointer to the query result obtained.
+ * @param query Attributes of the asset to query.
+ * @param queryCnt Number of attributes.
+ * @param resultSet Array of query results.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
  *         1. Incorrect parameter types.
@@ -183,11 +181,11 @@ int32_t OH_Asset_PreQuery(const Asset_Attr *query, uint32_t queryCnt, Asset_Blob
 int32_t OH_Asset_Query(const Asset_Attr *query, uint32_t queryCnt, Asset_ResultSet *resultSet);
 
 /**
- * @brief Processes data after the query of the asset that requires user authentication.
+ * @brief Performs postprocessing for the asset query. This API is used when user authentication is required for the
+ * access to the asset.
  *
- * @param handle Pointer to the handle of the data to process, which includes the challenge value returned by
- *     <b>OH_Asset_PreQuery</b>.
- * @param handleCnt Number of the elements in the handle attribute set.
+ * @param handle Handle of the query operation, including the challenge value returned by {@link OH_Asset_PreQuery}.
+ * @param handleCnt Number of elements in the handle attribute set.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_INVALID_ARGUMENT} 401 - Parameter error. Possible causes:
  *         1. Mandatory parameters are left unspecified.
@@ -204,11 +202,11 @@ int32_t OH_Asset_Query(const Asset_Attr *query, uint32_t queryCnt, Asset_ResultS
 int32_t OH_Asset_PostQuery(const Asset_Attr *handle, uint32_t handleCnt);
 
 /**
- * @brief Query the result of synchronization.
+ * @brief Queries the sync result of an asset.
  *
- * @param query Pointer to the search criteria.
- * @param queryCnt Number of the search criteria.
- * @param syncResult Pointer to the synchronization result obtained.
+ * @param query Attributes of the asset to query the sync result.
+ * @param queryCnt Number of attributes.
+ * @param syncResult Sync result of the queried asset.
  * @return {@link ASSET_SUCCESS} 0 - The operation is successful.
  *     {@link ASSET_SERVICE_UNAVAILABLE} 24000001 - The ASSET service is unavailable.
  *     {@link ASSET_OUT_OF_MEMORY} 24000006 - Insufficient memory.
@@ -223,10 +221,10 @@ int32_t OH_Asset_PostQuery(const Asset_Attr *handle, uint32_t handleCnt);
 int32_t OH_Asset_QuerySyncResult(const Asset_Attr *query, uint32_t queryCnt, Asset_SyncResult *syncResult);
 
 /**
- * @brief Parses the query result to obtain the specified attribute value.
+ * @brief Parses the query result and obtains the specified attribute.
  *
- * @param result Pointer to the query result to parse, which is obtained by <b>OH_Asset_Query</b>.
- * @param tag Tag of the attribute to obtain.
+ * @param result Query result returned by {@link OH_Asset_Query}.
+ * @param tag Key of the attribute to obtain.
  * @return Returns <b>Asset_Attr</b> obtained if the operation is successful; returns <b>NULL</b> otherwise.
  *     The attribute does not need to be released by the service.
  * @since 11
@@ -236,7 +234,7 @@ Asset_Attr *OH_Asset_ParseAttr(const Asset_Result *result, Asset_Tag tag);
 /**
  * @brief Releases the memory occupied by the challenge value.
  *
- * @param blob Pointer to the challenge value (obtained by <b>OH_Asset_PreQuery</b>) to release.
+ * @param blob Challenge value returned by {@link OH_Asset_PreQuery}.
  * @since 11
  */
 void OH_Asset_FreeBlob(Asset_Blob *blob);
@@ -244,11 +242,10 @@ void OH_Asset_FreeBlob(Asset_Blob *blob);
 /**
  * @brief Releases the memory occupied by the query result.
  *
- * @param resultSet Pointer to the query result (obtained by <b>OH_Asset_Query</b>) to release.
+ * @param resultSet Query result returned by {@link OH_Asset_Query}.
  * @since 11
  */
 void OH_Asset_FreeResultSet(Asset_ResultSet *resultSet);
-
 #ifdef __cplusplus
 }
 #endif
