8b14e74c7cb19620dd117fc6f2219a7b80865a48
gcw_RYdx6Zzw <zhongjie35@huawei.com>
2026-05-13 16:33:14 +0800

    update: 更新文件 dlp_permission_api.h
    Signed-off-by: gcw_RYdx6Zzw <zhongjie35@huawei.com>

--- zh-cn/DataProtectionKit/dlp_permission_api.h
+++ zh-cn/DataProtectionKit/dlp_permission_api.h
@@ -25,7 +25,7 @@
 /**
  * @file dlp_permission_api.h
  *
- * @brief Defines the APIs for cross-device file access management, encrypted storage, and access authorization.
+ * @brief 声明用于跨设备的文件的权限管理、加密存储、授权访问等能力的接口。
  *
  * @library libohdlp_permission.so
  * @kit DataProtectionKit
@@ -44,37 +44,37 @@ extern "C" {
 #endif
 
 /**
- * @brief Enumerates the DLP error codes.
+ * @brief DLP错误码的枚举。
  *
  * @since 14
  */
 typedef enum {
     /**
-     * The operation is successful.
+     * 表示操作成功。
      */
     ERR_OH_SUCCESS = 0,
     /**
-     * Invalid parameters are specified.
+     * 表示入参错误。
      */
     ERR_OH_INVALID_PARAMETER = 19100001,
     /**
-     * The caller is not a DLP sandbox application.
+     * 表示非DLP沙箱应用。
      */
     ERR_OH_API_ONLY_FOR_SANDBOX = 19100006,
     /**
-     * The API is not available to a DLP sandbox application.
+     * 表示DLP沙箱应用不允许调用此接口。
      */
     ERR_OH_API_NOT_FOR_SANDBOX = 19100007,
     /**
-     * The system service is abnormal.
+     * 表示系统服务工作异常。
      */
     ERR_OH_SYSTEM_SERVICE_EXCEPTION = 19100011,
     /**
-     * The memory allocation fails.
+     * 表示内存申请失败。
      */
     ERR_OH_OUT_OF_MEMORY = 19100012,
     /**
-     * The application is not authorized to perform the operation.
+     *表示应用未授权。
      */
     ERR_OH_APPLICATION_NOT_AUTHORIZED = 19100018,
 } DLP_ErrCode;
@@ -86,112 +86,104 @@ typedef enum {
  */
 typedef enum {
     /**
-     * No permission on the file.
+     * 表示无文件权限。
      */
     NO_PERMISSION = 0,
     /**
-     * Read-only permission.
+     * 表示文件的只读权限。
      */
     READ_ONLY = 1,
     /**
-     * Editing permission.
+     * 表示文件的编辑权限。
      */
     CONTENT_EDIT = 2,
     /**
-     * Full control.
+     * 表示文件的完全控制权限。
      */
     FULL_CONTROL = 3
 } DLP_FileAccess;
 
 /**
- * @brief Obtains the permission information of this DLP file.
+ * @brief 查询当前DLP沙箱的权限信息。
  *
- * @param dlpFileAccess User permission on the DLP file, for example, read-only.
- * @param flags Pointer to the operation permissions allowed for the DLP file. The options are as follows:
- *     <br>**0x00000000** indicates no permission on the file.
- *     <br>**0x00000001** indicates the permission for viewing the file.
- *     <br>**0x00000002** indicates the permission for saving the file.
- *     <br>**0x00000004** indicates the permission for saving the file as another file.
- *     <br>**0x00000008** indicates the permission for editing the file.
- *     <br>**0x00000010** indicates the permission for capturing screenshots of the file.
- *     <br>**0x00000020** indicates the permission for sharing the screen, on which the file is open.
- *     <br>**0x00000040** indicates the permission for recording the screen, on which the file is open.
- *     <br>**0x00000080** indicates the permission for copying the file.
- *     <br>**0x00000100** indicates the permission for printing the file.
- *     <br>**0x00000200** indicates the permission for exporting the file.
- *     <br>**0x00000400** indicates the permission for modifying the permissions on the file.
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_INVALID_PARAMETER} 19100001 - If the parameter value is invalid.
- *     {@link DLP_ErrCode#ERR_OH_API_ONLY_FOR_SANDBOX} 19100006 - If no permission to
- *     call this API, which is available only for DLP sandbox applications.
- *     {@link DLP_ErrCode#ERR_OH_SYSTEM_SERVICE_EXCEPTION} 19100011 - If the system ability
- *     works abnormally.
- *     {@link DLP_ErrCode#ERR_OH_OUT_OF_MEMORY} 19100012 - If the memory error.
+ * @param dlpFileAccess 表示DLP文件针对用户的授权类型，例如：只读。
+ * @param flags 表示DLP文件的详细操作权限，操作权限的具体含义为：
+ *     <br>**0x00000000** 表示无文件权限。
+ *     <br>**0x00000001** 表示文件的查看权限。
+ *     <br>**0x00000002** 表示文件的保存权限。
+ *     <br>**0x00000004** 表示文件的另存为权限。
+ *     <br>**0x00000008** 表示文件的编辑权限。
+ *     <br>**0x00000010** 表示文件的截屏权限。
+ *     <br>**0x00000020** 表示文件的共享屏幕权限。
+ *     <br>**0x00000040** 表示文件的录屏权限。
+ *     <br>**0x00000080** 表示文件的复制权限。
+ *     <br>**0x00000100** 表示文件的打印权限。
+ *     <br>**0x00000200** 表示文件的导出权限。
+ *     <br>**0x00000400** 表示文件的修改文件权限。
+ * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - 操作成功。
+ *     <br>19100001 -入参错误。
+ *     <br>19100006 - 非DLP沙箱应用。
+ *     <br>19100011 - 系统服务工作异常。
+ *     <br>19100012 - 内存申请失败。
  * @since 14
  */
 DLP_ErrCode OH_DLP_GetDlpPermissionInfo(DLP_FileAccess *dlpFileAccess, uint32_t *flags);
 
 /**
- * @brief Obtains the original file name of a DLP file.
+ * @brief 获取指定DLP文件名的原始文件名。
  *
- * @param fileName Pointer to the target file whose original file name is to be obtained.
- * @param originalFileName Double pointer to the original file name obtained.
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_INVALID_PARAMS} 19100001 - If the parameter value is invalid.
- *     {@link DLP_ErrCode#ERR_OH_OUT_OF_MEMORY} 19100012 - If the memory error.
+ * @param fileName 指定要查询的文件名。
+ * @param originalFileName  DLP文件的原始文件名。
+ * @return 0 - 操作成功。
+ *     <br>19100001 -入参错误。
+ *     <br>19100012 - 内存申请失败。
  * @since 14
  */
 DLP_ErrCode OH_DLP_GetOriginalFileName(const char *fileName, char **originalFileName);
 
 /**
- * @brief Checks whether this application is running in a DLP sandbox environment.
+ * @brief 查询当前应用是否运行在DLP沙箱环境。
  *
- * @param isInSandbox Returns **true** if the application is running in a DLP sandbox; returns **false** otherwise.
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_SYSTEM_SERVICE_EXCEPTION} 19100011 - If the system ability
- *     works abnormally.
- *     {@link DLP_ErrCode#ERR_OH_OUT_OF_MEMORY} 19100012 - If the memory error.
+ * @param isInSandbox true表示当前应用运行在DLP沙箱环境，false表示当前应用不是运行在DLP沙箱环境。
+ * @return 0 - 操作成功。
+ *     <br>19100011 - 系统服务工作异常。
+ *     <br>19100012 - 内存申请失败。
  * @since 14
  */
 DLP_ErrCode OH_DLP_IsInSandbox(bool *isInSandbox);
 
 /**
- * @brief Sets sandbox application configuration.
+ * @brief 设置沙箱应用配置信息。
  *
- * @param configInfo Pointer to the sandbox application configuration obtained.
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_INVALID_PARAMETER} 19100001 - If the parameter value is invalid.
- *     {@link DLP_ErrCode#ERR_OH_API_NOT_FOR_SANDBOX} 19100007 - If no permission to
- *     call this API, which is available only for non-DLP sandbox applications.
- *     {@link DLP_ErrCode#ERR_OH_SYSTEM_SERVICE_EXCEPTION} 19100011 - If the system ability
- *     works abnormally.
- *     {@link DLP_ErrCode#ERR_OH_APPLICATION_NOT_AUTHORIZED} 19100018 - If the application is not authorized.
+ * @param configInfo 沙箱应用配置信息。
+ * @return 0 - 操作成功。
+ *     <br>19100001 - 入参错误。
+ *     <br>19100007 - DLP沙箱应用不允许调用此接口。
+ *     <br>19100011 - 系统服务工作异常。
+ *     <br>19100018 - 应用未授权。
  * @since 14
  */
 DLP_ErrCode OH_DLP_SetSandboxAppConfig(const char *configInfo);
 
 /**
- * @brief Obtains the sandbox application configuration.
+ * @brief 获取沙箱应用配置信息。
  *
- * @param configInfo Pointer to the sandbox application configuration obtained.
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_SYSTEM_SERVICE_EXCEPTION} 19100011 - If the system ability
- *     works abnormally.
- *     {@link DLP_ErrCode#ERR_OH_OUT_OF_MEMORY} 19100012 - If the memory error.
- *     {@link DLP_ErrCode#ERR_OH_APPLICATION_NOT_AUTHORIZED} 19100018 - If the application is not authorized.
+ * @param configInfo 沙箱应用配置信息。
+ * @return 0 - 操作成功。
+ *     <br>19100011 - 系统服务工作异常。
+ *     <br>119100012 - 内存申请失败。
+ *     <br>119100018 - 应用未授权。
  * @since 14
  */
 DLP_ErrCode OH_DLP_GetSandboxAppConfig(char **configInfo);
 
 /**
- * @brief Cleans the sandbox application configuration.
+ * @brief 清理沙箱应用配置信息。
  *
- * @return {@link DLP_ErrCode#ERR_OH_SUCCESS} 0 - If the operation is successful.
- *     {@link DLP_ErrCode#ERR_OH_API_NOT_FOR_SANDBOX} 19100007 - If no permission to
- *     call this API, which is available only for non-DLP sandbox applications.
- *     {@link DLP_ErrCode#ERR_OH_SYSTEM_SERVICE_EXCEPTION} 19100011 - If the system ability
- *     works abnormally.
- *     {@link DLP_ErrCode#ERR_OH_APPLICATION_NOT_AUTHORIZED} 19100018 - If the application is not authorized.
+ * @return 0 - 操作成功。
+ *     <br>19100007 - DLP沙箱应用不允许调用此接口。
+ *     <br>19100011 - 系统服务工作异常。
+ *     <br>19100018 - 应用未授权。
  * @since 14
  */
 DLP_ErrCode OH_DLP_CleanSandboxAppConfig();
