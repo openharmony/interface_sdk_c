35bce0d0a3e91fea0bf05dc6ffce50b423858efd
Oujinyang <oujinyang@h-partners.com>
2026-05-14 15:34:28 +0800

    蓝黄同步
    Signed-off-by: Oujinyang <oujinyang@h-partners.com>

--- multimedia/player_framework/avimage_generator_base.h
+++ multimedia/player_framework/avimage_generator_base.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @addtogroup AVImageGenerator
  * @{
@@ -22,13 +21,11 @@
  * @syscap SystemCapability.Multimedia.Media.AVImageGenerator
  * @since 18
  */
-
 /**
  * @file avimage_generator_base.h
  *
- * @brief Defines the structure and enumeration for AVImageGenerator.
- *
- * @syscap SystemCapability.Multimedia.Media.AVImageGenerator
+ * @brief The file declares the enums used by the AVImageGenerator.
+ * 
  * @kit MediaKit
  * @library libavimage_generator.so
  * @since 18
@@ -44,30 +41,25 @@ extern "C" {
 #endif
 
 /**
- * @brief Enumerates the image query options about the relationship between the given timeUs and a key frame.
- *
- * @syscap SystemCapability.Multimedia.Media.AVImageGenerator
+ * @brief Enumerates the mappings between time points and video frames.
+ * 
  * @since 18
  */
 typedef enum OH_AVImageGenerator_QueryOptions {
     /**
-     * This option is used to fetch a key frame from the given media
-     * resource that is located right after or at the given time.
+     * Extracts the key frame at or next to the specified time.
      */
     OH_AVIMAGE_GENERATOR_QUERY_NEXT_SYNC = 0,
     /**
-     * This option is used to fetch a key frame from the given media
-     * resource that is located right before or at the given time.
+     * Extracts the key frame at or prior to the specified time.
      */
     OH_AVIMAGE_GENERATOR_QUERY_PREVIOUS_SYNC = 1,
     /**
-     * This option is used to fetch a key frame from the given media
-     * resource that is located closest to or at the given time.
+     * Extracts the key frame closest to the specified time.
      */
     OH_AVIMAGE_GENERATOR_QUERY_CLOSEST_SYNC = 2,
     /**
-     * This option is used to fetch a frame (maybe not keyframe) from
-     * the given media resource that is located closest to or at the given time.
+     * Extracts the frame (not necessarily a key frame) closest to the specified time.
      */
     OH_AVIMAGE_GENERATOR_QUERY_CLOSEST = 3,
 } OH_AVImageGenerator_QueryOptions;
