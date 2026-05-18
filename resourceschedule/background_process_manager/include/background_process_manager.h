4ca5a9e983208cf4414eb355662371c1d007134c
buzhi_ <wangzhen350@huawei.com>
2026-05-18 22:15:36 +0800

    注释一致性整改
    Signed-off-by: buzhi_ <wangzhen350@huawei.com>

--- resourceschedule/background_process_manager/include/background_process_manager.h
+++ resourceschedule/background_process_manager/include/background_process_manager.h
@@ -27,7 +27,7 @@
  * use these APIs to suppress and unsuppress child processes to prevent child processes from occupying too many system
  * resources and causing system stuttering. The APIs take effect only for the child processes created through
  * {@link OH_Ability_StartNativeChildProcess}.
- * 
+ *
  * @library libbackground_process_manager.z.so
  * @kit BackgroundTasksKit
  * @syscap SystemCapability.Resourceschedule.BackgroundProcessManager
@@ -43,7 +43,7 @@ extern "C" {
 
 /**
  * @brief Enumerates child process priorities.
- * 
+ *
  * @since 17
  */
 typedef enum BackgroundProcessManager_ProcessPriority {
@@ -51,7 +51,7 @@ typedef enum BackgroundProcessManager_ProcessPriority {
      * @brief Compared with **PROCESS_INACTIVE**, this priority has a more obvious suppression effect. Child processes
      *     can obtain less CPU resources. You are advised to set this priority when executing background child
      *     processes that cannot be perceived by users, such as background image-text pages.
-     * 
+     *
      * @since 17
      */
     PROCESS_BACKGROUND = 1,
@@ -59,7 +59,7 @@ typedef enum BackgroundProcessManager_ProcessPriority {
     /**
      * @brief You are advised to set this priority when executing background child processes that can be perceived by
      *     users, such as audio playback and navigation.
-     * 
+     *
      * @since 17
      */
     PROCESS_INACTIVE = 2,
@@ -67,13 +67,13 @@ typedef enum BackgroundProcessManager_ProcessPriority {
 
 /**
  * @brief Enumerates the error codes used by the background child process management.
- * 
+ *
  * @since 17
  */
 typedef enum BackgroundProcessManager_ErrorCode {
     /**
      * @error result is OK.
-     * 
+     *
      * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS = 0,
@@ -81,7 +81,7 @@ typedef enum BackgroundProcessManager_ErrorCode {
     /**
      * @error invalid parameter. Possible causes:
      * 1. priority is out of range.
-     * 
+     *
      * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_INVALID_PARAM = 401,
@@ -89,7 +89,7 @@ typedef enum BackgroundProcessManager_ErrorCode {
     /**
      * @error remote error. Possible causes:
      * 1. remote is not work.
-     * 
+     *
      * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_REMOTE_ERROR = 31800001,
@@ -99,7 +99,7 @@ typedef enum BackgroundProcessManager_ErrorCode {
  * @brief Sets the child process priority. After a child process is suppressed, the CPU resources that can be obtained
  *     will be limited. If the scheduling policy of the main process changes, for example, from the background to the
  *     foreground, the child process changes with the main process. To suppress the child process, call this API again.
- * 
+ *
  * @param pid ID of the child process to be suppressed, which is the value of the **pid** parameter after the child
  *     process is created through the {@link OH_Ability_StartNativeChildProcess} API.
  * @param priority Suppression priority.
@@ -114,7 +114,7 @@ int OH_BackgroundProcessManager_SetProcessPriority(int pid, BackgroundProcessMan
  *     process. If the scheduling policy of the main process changes, for example, from the background to the
  *     foreground, the child process changes with the main process. The effect is the same as calling
  *     **resetProcessPriority**.
- * 
+ *
  * @param pid ID of the child process, which is the value of the **pid** parameter of the
  * {@link OH_Ability_StartNativeChildProcess} API.
  * @return {@link ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS} is returned if the suppression parameter is sent successfully
