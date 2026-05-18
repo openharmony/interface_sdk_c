b98112a02a32bdf5107c49cdb1026bb7eecf878b
buzhi_ <wangzhen350@huawei.com>
2026-05-18 21:48:06 +0800

    注释一致性整改
    Signed-off-by: buzhi_ <wangzhen350@huawei.com>

--- resourceschedule/background_process_manager/include/background_process_manager.h
+++ resourceschedule/background_process_manager/include/background_process_manager.h
@@ -48,14 +48,19 @@ extern "C" {
  */
 typedef enum BackgroundProcessManager_ProcessPriority {
     /**
-     * @brief Compared with **PROCESS_INACTIVE**, this priority has a more obvious suppression effect. Child processes can obtain less CPU resources. You are advised to set this priority when executing background child processes that cannot be perceived by users, such as background image-text pages.
+     * @brief Compared with **PROCESS_INACTIVE**, this priority has a more obvious suppression effect. Child processes
+     *     can obtain less CPU resources. You are advised to set this priority when executing background child 
+     *     processes that cannot be perceived by users, such as background image-text pages.
      * 
+     * @since 17
      */
     PROCESS_BACKGROUND = 1,
 
     /**
-     * @brief You are advised to set this priority when executing background child processes that can be perceived by users, such as audio playback and navigation.
+     * @brief You are advised to set this priority when executing background child processes that can be perceived by
+     *     users, such as audio playback and navigation.
      * 
+     * @since 17
      */
     PROCESS_INACTIVE = 2,
 } BackgroundProcessManager_ProcessPriority;
@@ -68,31 +73,37 @@ typedef enum BackgroundProcessManager_ProcessPriority {
 typedef enum BackgroundProcessManager_ErrorCode {
     /**
      * @error result is OK.
+     * 
+     * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS = 0,
 
     /**
      * @error invalid parameter. Possible causes:
      * 1. priority is out of range.
+     * 
+     * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_INVALID_PARAM = 401,
 
     /**
      * @error remote error. Possible causes:
      * 1. remote is not work.
+     * 
+     * @since 17
      */
     ERR_BACKGROUND_PROCESS_MANAGER_REMOTE_ERROR = 31800001,
 } BackgroundProcessManager_ErrorCode;
 
 /**
  * @brief Sets the child process priority. After a child process is suppressed, the CPU resources that can be obtained
- * will be limited. If the scheduling policy of the main process changes, for example, from the background to the
- * foreground, the child process changes with the main process. To suppress the child process, call this API again.
+ *     will be limited. If the scheduling policy of the main process changes, for example, from the background to the
+ *     foreground, the child process changes with the main process. To suppress the child process, call this API again.
  * 
  * @param pid ID of the child process to be suppressed, which is the value of the **pid** parameter after the child
- * process is created through the {@link OH_Ability_StartNativeChildProcess} API.
+ *     process is created through the {@link OH_Ability_StartNativeChildProcess} API.
  * @param priority Suppression priority.
- * @return {@link ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS} is returned if the suppression parameter is sent successfully.
+ * @return {@link ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS} is returned if the suppression parameter is sent successfully
  * {@link ERR_BACKGROUND_PROCESS_MANAGER_INVALID_PARAM} is returned if the parameter check fails.
  * @since 17
  */
@@ -100,12 +111,13 @@ int OH_BackgroundProcessManager_SetProcessPriority(int pid, BackgroundProcessMan
 
 /**
  * @brief Unsuppresses the child process. In this case, the child process follows the scheduling policy of the main
- * process. If the scheduling policy of the main process changes, for example, from the background to the foreground,
- * the child process changes with the main process. The effect is the same as calling **resetProcessPriority**.
+ *     process. If the scheduling policy of the main process changes, for example, from the background to the 
+ *     foreground, the child process changes with the main process. The effect is the same as calling 
+ *     **resetProcessPriority**.
  * 
  * @param pid ID of the child process, which is the value of the **pid** parameter of the
  * {@link OH_Ability_StartNativeChildProcess} API.
- * @return {@link ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS} is returned if the suppression parameter is sent successfully.
+ * @return {@link ERR_BACKGROUND_PROCESS_MANAGER_SUCCESS} is returned if the suppression parameter is sent successfully
  * @since 17
  */
 int OH_BackgroundProcessManager_ResetProcessPriority(int pid);
