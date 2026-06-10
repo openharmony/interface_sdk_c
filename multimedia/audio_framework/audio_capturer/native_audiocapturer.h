eb61f9f75b726c5563910c18215631e13e38166c
wei-yuantian <weiyuantian1@huawei.com>
2026-06-10 15:17:25 +0800

    update: 更新文件 native_audiocapturer.h
    Signed-off-by: wei-yuantian <weiyuantian1@huawei.com>

--- multimedia/audio_framework/audio_capturer/native_audiocapturer.h
+++ multimedia/audio_framework/audio_capturer/native_audiocapturer.h
@@ -440,16 +440,16 @@ OH_AudioStream_Result OH_AudioCapturer_SetIndependentAudioSessionStrategy(
     OH_AudioCapturer* capturer, const OH_AudioSession_Strategy* strategy, uint32_t behavior);
 
 /**
- * @brief Callback when cellular call recording risk warning is finished, developer must wait for
- * this state become true before start cellular call recording.
+ * @brief Callback when cellular call recording risk warning is finished,
+ * developer must wait for this state become true before start cellular call recording.
  *
  * @param capturer AudioCapturer where this event occurs.
  * @param userData User data which is passed by user.
  * @param riskWarningFinished Indicates that whether cellular call recording risk warning is finished.
  * @since 26.0.0
- */
-typedef void (*OH_AudioCapturer_CellularRecordRiskWarningCallback)(OH_AudioCapturer* capturer,
-    void* userData, bool riskWarningFinished);
+ */     
+typedef void (*OH_AudioCapturer_SensitiveRecordPermitCallback)(
+    OH_AudioCapturer* capturer, void* userData, bool isPermited);
 #ifdef __cplusplus
 }
 #endif
