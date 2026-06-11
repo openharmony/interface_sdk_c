849978dae43cb6845031f8154bc8b20398fe4eee
wei-yuantian <weiyuantian1@huawei.com>
2026-06-11 17:00:11 +0800

    update: 更新文件 native_audiostream_base.h
    Signed-off-by: wei-yuantian <weiyuantian1@huawei.com>

--- multimedia/audio_framework/common/native_audiostream_base.h
+++ multimedia/audio_framework/common/native_audiostream_base.h
@@ -556,7 +556,7 @@ typedef enum {
     /**
      * Voice call downlink source type.
      * When using OH_AudioStreamBuilder_GenerateCapturer to create the stream,
-     * it is necessary to verify the ohos.permission.CAPTURE_PLAYBACK_DWONLINK permission,
+     * it is necessary to verify the ohos.permission.CAPTURE_PLAYBACK_DOWNLINK permission,
      * when the permission is not available, it will return {@link AUDIOSTREAM_ERROR_INVALID_PARAM}.
      *
      * @since 26.0.0
