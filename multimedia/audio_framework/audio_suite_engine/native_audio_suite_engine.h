dfb39137faa2aa637dcf07c63764827e42804bf6
renzhaohu <renzhaohu1@h-partners.com>
2026-06-22 17:23:55 +0800

    修改API告警5
    Signed-off-by: renzhaohu <renzhaohu1@h-partners.com>

--- multimedia/audio_framework/audio_suite_engine/native_audio_suite_engine.h
+++ multimedia/audio_framework/audio_suite_engine/native_audio_suite_engine.h
@@ -74,7 +74,7 @@ OH_AudioSuite_Result OH_AudioSuiteEngine_Destroy(OH_AudioSuiteEngine* audioSuite
  * the engine can create multiple pipelines, and one pipeline must include at least one input node and one output node.
  * When the pipeline operates in {@link #AUDIOSUITE_PIPELINE_EDIT_MODE}, it supports all effect nodes.
  * When the pipeline operates in {@link #AUDIOSUITE_PIPELINE_REALTIME_MODE},
- * before API version 23, it only supports the {@link EFFECT_NODE_TYPE_EQUALIZER} effect node.
+ * before API version 23, it only supports the {@link EFFECT_NODE_TYPE_EQUALIZER} effect node,
  * in API version 23 and later, it supports all effect node.
  *
  * @param audioSuiteEngine Reference created by OH_AudioSuiteEngine_Create.
