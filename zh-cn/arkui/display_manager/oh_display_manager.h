2791fb14634b35318e54329aa42c492c3ccecd59
王青青 <wangqingqing55@h-partners.com>
2026-06-26 17:11:30 +0800

    注释格式同步架构平台
    Signed-off-by: 王青青 <wangqingqing55@h-partners.com>

--- zh-cn/arkui/display_manager/oh_display_manager.h
+++ zh-cn/arkui/display_manager/oh_display_manager.h
@@ -50,8 +50,8 @@ extern "C" {
  *
  * @param displayId 默认屏幕的id号，非负整数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayId(uint64_t *displayId);
@@ -61,8 +61,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayId(uint6
  *
  * @param displayWidth 默认屏幕的宽度，单位为px，该参数应为整数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayWidth(int32_t *displayWidth);
@@ -72,8 +72,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayWidth(in
  *
  * @param displayHeight 默认屏幕的高度，单位为px，该参数应为整数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayHeight(int32_t *displayHeight);
@@ -83,8 +83,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayHeight(i
  *
  * @param displayRotation 默认屏幕的顺时针旋转角度，具体可见{@link NativeDisplayManager_Rotation}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayRotation(
@@ -95,8 +95,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayRotation
  *
  * @param displayOrientation 屏幕当前显示的方向，具体可见{@link NativeDisplayManager_Orientation}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayOrientation(
@@ -107,8 +107,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayOrientat
  *
  * @param virtualPixels 屏幕的虚拟像素密度，该参数为浮点数，通常与densityPixels相同，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayVirtualPixelRatio(float *virtualPixels);
@@ -118,8 +118,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayVirtualP
  *
  * @param refreshRate 屏幕的刷新率，该参数应为整数，单位为Hz，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayRefreshRate(uint32_t *refreshRate);
@@ -129,8 +129,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayRefreshR
  *
  * @param densityDpi 屏幕的物理像素密度，表示每英寸上的像素点数。该参数为整数，单位为px，实际能取到的值取决于不同设备设置里提供的可选值。此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityDpi(int32_t *densityDpi);
@@ -141,8 +141,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityD
  * @param densityPixels 设备逻辑像素的密度，代表物理像素与逻辑像素的缩放系数，该参数为浮点数，受densityDPI范围限制，取值范围在[0.5，4.0]。一般取值1.0、3.0等，
  *     实际取值取决于不同设备提供的densityDpi。此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityPixels(float *densityPixels);
@@ -152,8 +152,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityP
  *
  * @param scaledDensity 显示字体的缩放因子，该参数为浮点数，通常与densityPixels相同，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayScaledDensity(float *scaledDensity);
@@ -163,8 +163,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayScaledDe
  *
  * @param xDpi X方向中每英寸屏幕的物理像素值，该参数为浮点数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityXdpi(float *xDpi);
@@ -174,8 +174,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityX
  *
  * @param yDpi 获取Y方向中每英寸屏幕的物理像素值，该参数为浮点数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityYdpi(float *yDpi);
@@ -185,8 +185,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDefaultDisplayDensityY
  *
  * @param cutoutInfo 挖孔屏、刘海屏、瀑布屏等不可用屏幕区域信息，具体可见{@link NativeDisplayManager_CutoutInfo}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateDefaultDisplayCutoutInfo(
@@ -197,8 +197,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateDefaultDisplayCutou
  *
  * @param cutoutInfo 销毁通过{@link OH_NativeDisplayManager_CreateDefaultDisplayCutoutInfo}接口获取的挖孔屏、刘海屏、瀑布屏等不可用屏幕区域信息对象，
  *     具体可见{@link NativeDisplayManager_CutoutInfo}。
-* @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_DestroyDefaultDisplayCutoutInfo(
@@ -217,8 +217,8 @@ bool OH_NativeDisplayManager_IsFoldable();
  *
  * @param displayMode 折叠设备当前的显示模式，具体可见{@link NativeDisplayManager_FoldDisplayMode}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetFoldDisplayMode(
@@ -239,8 +239,8 @@ typedef void (*OH_NativeDisplayManager_DisplayChangeCallback)(uint64_t displayId
  * @param listenerIndex 注册成功后返回的监听编号，调用取消注册函数{@link OH_NativeDisplayManager_UnregisterDisplayChangeListener}时作为入参使用，
  *     此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayChangeListener(
@@ -251,8 +251,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayChangeList
  *
  * @param listenerIndex 调用注册函数{@link OH_NativeDisplayManager_RegisterDisplayChangeListener}时获取到的监听编号。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterDisplayChangeListener(uint32_t listenerIndex);
@@ -274,9 +274,9 @@ typedef void (*OH_NativeDisplayManager_FoldDisplayModeChangeCallback)(
  * @param listenerIndex 注册成功后返回的监听编号，调用取消注册函数{@link OH_NativeDisplayManager_UnregisterFoldDisplayModeChangeListener}
  *     时作为入参使用，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterFoldDisplayModeChangeListener(
@@ -287,9 +287,9 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterFoldDisplayModeCh
  *
  * @param listenerIndex 调用注册函数{@link OH_NativeDisplayManager_RegisterFoldDisplayModeChangeListener}时获取到的监听编号。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_DEVICE_NOT_SUPPORTED，表示该设备不支持此API。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 12
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterFoldDisplayModeChangeListener(uint32_t listenerIndex);
@@ -299,8 +299,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterFoldDisplayMode
  *
  * @param allDisplays 当前所有的屏幕信息，具体可见{@link NativeDisplayManager_DisplaysInfo}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 14
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateAllDisplays(
@@ -321,8 +321,8 @@ void OH_NativeDisplayManager_DestroyAllDisplays(NativeDisplayManager_DisplaysInf
  * @param displayId 指定屏幕的id编号，该值为非负整数。
  * @param displayInfo 指定的屏幕信息对象，具体可见{@link NativeDisplayManager_DisplayInfo}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 14
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateDisplayById(uint32_t displayId,
@@ -342,8 +342,8 @@ void OH_NativeDisplayManager_DestroyDisplay(NativeDisplayManager_DisplayInfo *di
  *
  * @param displayInfo 主屏的屏幕信息对象，具体可见{@link NativeDisplayManager_DisplayInfo}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_INVALID_PARAM，表示参数检查失败。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 14
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreatePrimaryDisplay(
@@ -365,8 +365,8 @@ typedef void (*OH_NativeDisplayManager_AvailableAreaChangeCallback)(uint64_t dis
  * @param listenerIndex 注册成功后返回的监听编号，
  *     调用取消注册函数{@link OH_NativeDisplayManager_UnregisterAvailableAreaChangeListener}时作为入参使用，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterAvailableAreaChangeListener(
@@ -378,8 +378,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterAvailableAreaChan
  * @param listenerIndex 调用注册函数
  *     {@link OH_NativeDisplayManager_RegisterAvailableAreaChangeListener}时获取到的监听编号。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterAvailableAreaChangeListener(uint32_t listenerIndex);
@@ -390,8 +390,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterAvailableAreaCh
  * @param displayId 查询屏幕的id号，非负整数。
  * @param availableArea 屏幕可用区域，具体可见{@link NativeDisplayManager_Rect}，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateAvailableArea(uint64_t displayId, NativeDisplayManager_Rect **availableArea);
@@ -401,8 +401,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_CreateAvailableArea(uint6
  *
  * @param availableArea 销毁通过{@link OH_NativeDisplayManager_CreateAvailableArea}获取的屏幕可用区域，
  *     可用区域定义具体可见{@link NativeDisplayManager_Rect}。
-* @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_DestroyAvailableArea(NativeDisplayManager_Rect *availableArea);
@@ -422,8 +422,8 @@ typedef void (*OH_NativeDisplayManager_DisplayAddCallback)(uint64_t displayId);
  * @param listenerIndex 注册成功后返回的监听编号，
  *     调用取消注册函数{@link OH_NativeDisplayManager_UnregisterDisplayAddListener}时作为入参使用，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayAddListener(
@@ -434,8 +434,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayAddListene
  *
  * @param listenerIndex 调用注册函数{@link OH_NativeDisplayManager_RegisterDisplayAddListener}时获取到的监听编号。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterDisplayAddListener(uint32_t listenerIndex);
@@ -455,8 +455,8 @@ typedef void (*OH_NativeDisplayManager_DisplayRemoveCallback)(uint64_t displayId
  * @param listenerIndex 注册成功后返回的监听编号，
  *     调用取消注册函数{@link OH_NativeDisplayManager_UnregisterDisplayRemoveListener}时作为入参使用，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayRemoveListener(
@@ -467,8 +467,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_RegisterDisplayRemoveList
  *
  * @param listenerIndex 调用注册函数{@link OH_NativeDisplayManager_RegisterDisplayRemoveListener}时获取到的监听编号。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM，表示非法参数。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterDisplayRemoveListener(uint32_t listenerIndex);
@@ -478,8 +478,8 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_UnregisterDisplayRemoveLi
  *
  * @param displayId 查询屏幕的id号，非负整数。
  * @param sourceMode 屏幕当前的显示模式，具体可见{@link NativeDisplayManager_SourceMode}，此处作为出参返回。
-* @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  * @since 20
  */
 NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDisplaySourceMode(uint64_t displayId, NativeDisplayManager_SourceMode *sourceMode);
@@ -491,7 +491,7 @@ NativeDisplayManager_ErrorCode OH_NativeDisplayManager_GetDisplaySourceMode(uint
  * @param x 相对于主屏左上角的x方向坐标，单位为px，该参数应为整数，此处作为出参返回。
  * @param y 相对于主屏左上角的y方向坐标，单位为px，该参数应为整数，此处作为出参返回。
  * @return 返回DISPLAY_MANAGER_OK，表示操作成功。
- *     返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
+ * 返回DISPLAY_MANAGER_ERROR_SYSTEM_ABNORMAL，表示系统服务工作异常。
  *     当前仅支持主屏幕和扩展屏幕查询屏幕位置信息，其他屏幕查询会返回DISPLAY_MANAGER_ERROR_ILLEGAL_PARAM。
  * @since 20
  */
