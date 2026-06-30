8d21cf3a248525bfc04c7b656a1514ac5dcf7200
Leal <tanqingliu@h-partners.com>
2026-06-30 16:24:10 +0800

    update
    Signed-off-by: Leal <tanqingliu@h-partners.com>

--- zh-cn/security/device_certificate/certmanager/include/cm_native_api.h
+++ zh-cn/security/device_certificate/certmanager/include/cm_native_api.h
@@ -42,27 +42,27 @@
 extern "C" {
 #endif
 /**
- * @brief 获取USB证书凭据的详情信息列表。调用完成后，需要调用OH_CertManager_FreeUkeyCertificate释放certificateList内存。
-*
+ * @brief 获取USB证书凭据的详情信息列表。
  * @param keyUri 存放USB证书凭据的唯一标识符（字符串格式）。
  * @param ukeyInfo USB证书凭据属性信息。
  * @param certificateList 获取到的USB证书凭据详情列表。
  * @return <ul>
-* <li>{@link OH_CM_ErrorCode} : </li>
-* <li>**OH_CM_SUCCESS = 0**: 操作成功。</li>
-* <li>**OH_CM_HAS_NO_PERMISSION = 201**: 权限校验失败。</li>
-* <li>**OH_CM_CAPABILITY_NOT_SUPPORTED = 801**: 设备不支持。</li>
-* <li>**OH_CM_PARAMETER_VALIDATION_FAILED = 17500011**: 入参校验失败。可能原因：
-* 1.参数格式错误。
-* 2.参数范围无效。</li>
-* <li>**OH_CM_INNER_FAILURE = 17500001**: 内部错误。可能原因：
-* 1.IPC通讯失败。
-* 2.内存操作错误。
-* 3.文件操作错误。</li>
-* <li>**OH_CM_NOT_FOUND = 17500002**: 证书不存在。</li>
-* <li>**OH_CM_ACCESS_UKEY_SERVICE_FAILED = 17500010**: USB证书凭据访问失败。</li>
-* </ul>
+ * <li>{@link OH_CM_ErrorCode} : </li>
+ * <li>**OH_CM_SUCCESS = 0**: 操作成功。</li>
+ * <li>**OH_CM_HAS_NO_PERMISSION = 201**: 权限校验失败。</li>
+ * <li>**OH_CM_CAPABILITY_NOT_SUPPORTED = 801**: 设备不支持。</li>
+ * <li>**OH_CM_PARAMETER_VALIDATION_FAILED = 17500011**: 入参校验失败。可能原因：
+ * 1.参数格式错误。
+ * 2.参数范围无效。</li>
+ * <li>**OH_CM_INNER_FAILURE = 17500001**: 内部错误。可能原因：
+ * 1.IPC通讯失败。
+ * 2.内存操作错误。
+ * 3.文件操作错误。</li>
+ * <li>**OH_CM_NOT_FOUND = 17500002**: 证书不存在。</li>
+ * <li>**OH_CM_ACCESS_UKEY_SERVICE_FAILED = 17500010**: USB证书凭据访问失败。</li>
+ * </ul>
  * @permission ohos.permission.ACCESS_CERT_MANAGER
+ * @release OH_CertManager_FreeUkeyCertificate {certificateList}
  * @since 22
  */
 int32_t OH_CertManager_GetUkeyCertificate(const OH_CM_Blob *keyUri,
@@ -86,29 +86,31 @@ int32_t OH_CertManager_GetUkeyCertificate(const OH_CM_Blob *keyUri,
  * <li>OH_CM_NOT_FOUND = 17500002 ：证书不存在。</li>
  * </ul>
  * @permission ohos.permission.ACCESS_CERT_MANAGER
+ * @release OH_CertManager_FreeCredential {certificate}
  * @since 22
  */
 int32_t OH_CertManager_GetPrivateCertificate(const OH_CM_Blob *keyUri, OH_CM_Credential *certificate);
 /**
- * @brief 获取特定用户公共证书凭据详细信息。调用完成后，需要调用OH_CertManager_FreeCredential释放certificate内存。
+ * @brief 获取特定用户公共证书凭据详细信息。
 *
  * @param keyUri 存放用户公共证书凭据的唯一标识符（字符串格式）。
  * @param certificate 获取到的用户公共证书凭据的详情。
  * @return <ul>
-* <li>{@link OH_CM_ErrorCode} ： </li>
-* <li>OH_CM_SUCCESS = 0 ：操作成功。</li>
-* <li>OH_CM_HAS_NO_PERMISSION = 201 ：权限校验失败。</li>
-* <li>OH_CM_PARAMETER_VALIDATION_FAILED = 17500011 ：入参校验失败。可能原因：
-* 1.参数格式错误。
-* 2.参数范围无效。</li>
-* <li>OH_CM_INNER_FAILURE = 17500001 ：内部错误。可能原因：
-* 1.IPC通讯失败。
-* 2.内存操作错误。
-* 3.文件操作错误。</li>
-* <li>OH_CM_NOT_FOUND = 17500002 ：证书不存在。</li>
-* <li>OH_CM_NO_AUTHORIZATION =  17500005 ：应用未经用户授权。</li>
-* </ul>
+ * <li>{@link OH_CM_ErrorCode} ： </li>
+ * <li>OH_CM_SUCCESS = 0 ：操作成功。</li>
+ * <li>OH_CM_HAS_NO_PERMISSION = 201 ：权限校验失败。</li>
+ * <li>OH_CM_PARAMETER_VALIDATION_FAILED = 17500011 ：入参校验失败。可能原因：
+ * 1.参数格式错误。
+ * 2.参数范围无效。</li>
+ * <li>OH_CM_INNER_FAILURE = 17500001 ：内部错误。可能原因：
+ * 1.IPC通讯失败。
+ * 2.内存操作错误。
+ * 3.文件操作错误。</li>
+ * <li>OH_CM_NOT_FOUND = 17500002 ：证书不存在。</li>
+ * <li>OH_CM_NO_AUTHORIZATION =  17500005 ：应用未经用户授权。</li>
+ * </ul>
  * @permission ohos.permission.ACCESS_CERT_MANAGER
+ * @release OH_CertManager_FreeCredential {certificate}
  * @since 22
  */
 int32_t OH_CertManager_GetPublicCertificate(const OH_CM_Blob *keyUri, OH_CM_Credential *certificate);
