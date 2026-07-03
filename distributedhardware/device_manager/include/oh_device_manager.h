386afb79ed0f14c63b08d15b985beb6327e9c8ff
daiyunlong <daiyunlong4@huawei.com>
2026-07-03 17:06:25 +0800

    修复getLocalDeviceName函数声明仅支持C++应用语法
    Co-authored-by: Agent
    Signed-off-by: daiyunlong <daiyunlong4@huawei.com>

--- distributedhardware/device_manager/include/oh_device_manager.h
+++ distributedhardware/device_manager/include/oh_device_manager.h
@@ -63,10 +63,18 @@ extern "C" {
   *                                            *localDeviceName is not nullptr.
   * @since 20
   */
-int32_t OH_DeviceManager_GetLocalDeviceName(char **localDeviceName, unsigned int &len);
+int32_t OH_DeviceManager_GetLocalDeviceName(char **localDeviceName, unsigned int *len);
 
 #ifdef __cplusplus
-};
+}
+#endif
+
+#ifdef __cplusplus
+
+static inline int32_t OH_DeviceManager_GetLocalDeviceName(char **localDeviceName, unsigned int &len)
+{
+    return OH_DeviceManager_GetLocalDeviceName(localDeviceName, &len);
+}
 #endif
 
 /** @} */
