50aa51b43a5984bdcf1d032180ff3de1a98c642d
daiyunlong <daiyunlong4@huawei.com>
2026-07-24 23:32:11 +0800

    feat: add C ABI for GetLocalDeviceName
    Sync the public NDK header with device_manager: add OH_DeviceManager_GetLocalDeviceNameC(char **, unsigned int *) using pointer output parameters. Mark the legacy OH_DeviceManager_GetLocalDeviceName (unsigned int &) as deprecated and point users to the new C ABI. Add the new symbol to libdevicemanager.ndk.json with first_introduced 26.
    
    Co-Authored-By: Agent
    Signed-off-by: daiyunlong <daiyunlong4@huawei.com>

--- distributedhardware/device_manager/include/oh_device_manager.h
+++ distributedhardware/device_manager/include/oh_device_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,7 +40,6 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif
 
 /**
   * @brief Obtains the display name of the local device.
@@ -62,8 +61,36 @@ extern "C" {
   *      Returns {@link ERR_INVALID_PARAMETER} is returned if the localDeviceName is nullptr or
   *                                            *localDeviceName is not nullptr.
   * @since 20
+  * @deprecated since 20
+  * @useinstead OH_DeviceManager_GetLocalDeviceNameC
   */
 int32_t OH_DeviceManager_GetLocalDeviceName(char **localDeviceName, unsigned int &len);
+
+#endif
+
+/**
+  * @brief Obtains the display name of the local device.
+  * The device display name involves user privacy.
+  * You need to provide a privacy statement to declare the purpose of the device display name.
+  *
+  * @permission ohos.permission.READ_LOCAL_DEVICE_NAME
+  * @param localDeviceName Pointer to the display name of the local device.
+  *     After using this API, you need to manually release resources to free up space.
+  *     If the application has the ohos.permission.READ_LOCAL_DEVICE_NAME permission,
+  *     the device display name is returned.
+  *     Otherwise, the default device name is returned.
+  * @param len Pointer to the length of the display name of the local device.
+  *     After using this API, you need to manually release resources to free up space.
+  * @return Error code. For details about the error code definitions, see {@link DeviceManager_ErrorCode}.
+  *      Returns {@link ERR_OK} is returned if the execution is successful.
+  *      Returns {@link DM_ERR_FAILED} is returned if the function fails to be executed.
+  *      Returns {@link DM_ERR_OBTAIN_SERVICE} is returned if the device management service fails to be obtained.
+  *      Returns {@link DM_ERR_OBTAIN_BUNDLE_NAME} is returned if the bundle name fails to be obtained.
+  *      Returns {@link ERR_INVALID_PARAMETER} is returned if the localDeviceName is nullptr,
+  *                                            *localDeviceName is not nullptr, or len is nullptr.
+  * @since 26.0.0
+  */
+int32_t OH_DeviceManager_GetLocalDeviceNameC(char **localDeviceName, unsigned int *len);
 
 #ifdef __cplusplus
 };
