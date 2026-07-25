cf106d434121aa127a7dde97b481ed32f54b7add
mashaobo <mashaobo10@h-partners.com>
2026-07-25 11:19:47 +0800

    update: 更新文件 external_window.h
    Signed-off-by: mashaobo <mashaobo10@h-partners.com>

--- graphic/graphic_2d/native_window/external_window.h
+++ graphic/graphic_2d/native_window/external_window.h
@@ -958,8 +958,8 @@ int32_t OH_NativeWindow_UnlockAndFlushBuffer(OHNativeWindow* window);
  * @param metadata Indicates the pointer to a uint8_t vector.
  * @return {@link NATIVE_ERROR_OK} 0 - Success.
  *     {@link NATIVE_ERROR_INVALID_ARGUMENTS} 40001000 - window or metadata is NULL.
- *     {@link NATIVE_ERROR_UNKNOWN} 50002000 - Inner error.
- *     {@link NATIVE_ERROR_UNSUPPORTED} 50102000 - Unsupported metadata key.
+ *     {@link NATIVE_ERROR_UNKNOWN} 50002000 - failed to copy or allocate memory.
+ *     {@link NATIVE_ERROR_UNSUPPORTED} 50102000 - unsupported metadata key.
  * @since 26.0.0
  * @version 1.0
  */
@@ -977,8 +977,8 @@ int32_t OH_NativeWindow_Set3DMetadataValue(OHNativeWindow *window, OH_NativeBuff
  * @param metadata Indicates the pointer to a uint8_t vector.
  * @return {@link NATIVE_ERROR_OK} 0 - Success.
  *     {@link NATIVE_ERROR_INVALID_ARGUMENTS} 40001000 - window, metadata, or size is NULL.
- *     {@link NATIVE_ERROR_UNKNOWN} 50002000 - Inner error.
- *     {@link NATIVE_ERROR_UNSUPPORTED} 50102000 - Unsupported metadata key.
+ *     {@link NATIVE_ERROR_UNKNOWN} 50002000 - failed to copy or allocate memory.
+ *     {@link NATIVE_ERROR_UNSUPPORTED} 50102000 - unsupported metadata key.
  * @since 26.0.0
  * @version 1.0
  */
