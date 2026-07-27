2d7f1e3fec71f4cd5cf48f78f1e2d219cf7a1039
lcy_lovebug <lichengyi5@h-partners.com>
2026-07-27 19:44:10 +0800

    资料一致性问题修改
    Signed-off-by: lcy_lovebug <lichengyi5@h-partners.com>

--- zh-cn/security/huks/include/native_huks_external_crypto_api.h
+++ zh-cn/security/huks/include/native_huks_external_crypto_api.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2022-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,8 +17,7 @@
  * @addtogroup HuksExternalCryptoApi
  * @{
  *
- * @brief Describes the OpenHarmony Universal KeyStore (HUKS) capabilities specifical for external crypto extensions,
- * including provider management and ukey pin management and other operations, provided for applications.
+ * @brief 通用密钥库（HUKS）针对外部密钥管理扩展的能力，包括提供者管理、UKey PIN码管理及其它对应用提供的操作接口。
  *
  * @since 22
  */
@@ -27,12 +25,12 @@
 /**
  * @file native_huks_external_crypto_api.h
  *
- * @brief 定义面向外部密钥管理扩展的通用密钥库（HUKS）API。
+ * @brief 定义面向外部密钥管理扩展的通用密钥库（Universal KeyStore，HUKS）API。
  *
  * @library libhuks_external_crypto.z.so
  * @syscap SystemCapability.Security.Huks.CryptoExtension
  *
- *     include "huks/include/native_huks_external_crypto_type.h"
+ * @include <huks/native_huks_external_crypto_api.h>
  * @kit UniversalKeystoreKit
  * @since 22
  */
@@ -53,16 +51,16 @@ extern "C" {
  * @param providerName 指定提供者名称。
  * @param paramSet 指向注册参数的指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_PERMISSION_FAIL 201 - 权限校验失败，请先申请所需权限。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的API。
- *     <br>OH_HUKS_ERR_CODE_MISSING_CRYPTO_ALG_ARGUMENT 12000002 - 未能获取提供者参数。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - providerName或paramSet无效。
- *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000019 - 提供者已被注册。
- *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020 - 依赖模块发生错误。
- *     <br>OH_HUKS_ERR_CODE_EXCEED_LIMIT 12000025 - 提供者数量超过限制。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_PERMISSION_FAIL 201：权限校验失败，请先申请所需权限。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的API。
+ *     <br>OH_HUKS_ERR_CODE_MISSING_CRYPTO_ALG_ARGUMENT 12000002：未能获取提供者参数。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：providerName或paramSet无效。
+ *     <br>OH_HUKS_ERR_CODE_ITEM_EXISTS 12000019：提供者已被注册。
+ *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020：依赖模块发生错误。
+ *     <br>OH_HUKS_ERR_CODE_EXCEED_LIMIT 12000025：提供者数量超过限制。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_RegisterProvider(
@@ -73,16 +71,16 @@ struct OH_Huks_Result OH_Huks_RegisterProvider(
  *
  * @permission ohos.permission.CRYPTO_EXTENSION_REGISTER
  * @param providerName 指定提供者名称。
- * @param paramSet 指向注册参数的指针。
+ * @param paramSet 指向注销参数的指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_PERMISSION_FAIL 201 - 权限校验失败，请先申请所需权限。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的API。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011 - 未找到指定的提供者。
- *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR = 12000012 - 发生系统内部错误，密钥管理扩展模块没有加载。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - providerName无效。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_PERMISSION_FAIL 201：权限校验失败，请先申请所需权限。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的API。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011：未找到指定的提供者。
+ *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR = 12000012：发生系统内部错误，密钥管理扩展模块没有加载。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：providerName无效。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_UnregisterProvider(
@@ -95,18 +93,18 @@ struct OH_Huks_Result OH_Huks_UnregisterProvider(
  * @param resourceId 指定提供者的资源ID。
  * @param paramSet 指向句柄操作参数的指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的API。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006 - Ukey驱动报错。
- *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011 - 未找到缓存的资源句柄，需要先根据资源ID打开资源。
- *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012 - 发生系统内部错误，处理函数未找到。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_KEY_ALREADY_EXIST 12000017 - 资源已打开。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - resourceId或paramSet无效。
- *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020 - 提供者执行失败。
- *     <br>OH_HUKS_ERR_CODE_BUSY 12000024 - 提供者或Ukey忙。
- *     <br>OH_HUKS_ERR_CODE_EXCEED_LIMIT 12000025 - 打开资源的数量超过限制。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的API。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006：Key驱动报错。
+ *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011：未找到缓存的资源句柄，需要先根据资源ID打开资源。
+ *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012：发生系统内部错误，处理函数未找到。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_KEY_ALREADY_EXIST 12000017：资源已打开。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：resourceId或paramSet无效。
+ *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020：提供者执行失败。
+ *     <br>OH_HUKS_ERR_CODE_BUSY 12000024：提供者或UKey忙。
+ *     <br>OH_HUKS_ERR_CODE_EXCEED_LIMIT 12000025：打开资源的数量超过限制。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_OpenResource(
@@ -118,37 +116,37 @@ struct OH_Huks_Result OH_Huks_OpenResource(
  * @param resourceId 指定提供者的资源ID。
  * @param paramSet 指向句柄操作参数的指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的 API。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006 - Ukey驱动报错。
- *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012 - 发生系统内部错误，处理函数未找到。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - resourceId或paramSet无效。
- *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020 - 提供者执行失败。
- *     <br>OH_HUKS_ERR_CODE_BUSY 12000024 - 提供者或Ukey忙。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的 API。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006：UKey驱动报错。
+ *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012：发生系统内部错误，处理函数未找到。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：resourceId或paramSet无效。
+ *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020：提供者执行失败。
+ *     <br>OH_HUKS_ERR_CODE_BUSY 12000024：提供者或UKey忙。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_CloseResource(
     const struct OH_Huks_Blob *resourceId, const OH_Huks_ExternalCryptoParamSet *paramSet);
 
 /**
- * @brief 获取指定Ukey资源ID的PIN授权状态。
+ * @brief 获取指定UKey资源ID的PIN授权状态。
  *
  * @param resourceId 指定提供者的资源ID。
  * @param paramSet 指向PIN授权参数的指针。
  * @param authState 用于返回指定索引的授权状态。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的API。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006 - Ukey驱动报错。
- *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011 - 指定的资源ID无效。
- *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012 - 发生系统内部错误，处理函数未找到。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - resourceId或paramSet无效。
- *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020 - 提供者执行失败。
- *     <br>OH_HUKS_ERR_CODE_BUSY 12000024 - 提供者或Ukey忙。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的API。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006：UKey驱动报错。
+ *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011：指定的资源ID无效。
+ *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012：发生系统内部错误，处理函数未找到。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：resourceId或paramSet无效。
+ *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020：提供者执行失败。
+ *     <br>OH_HUKS_ERR_CODE_BUSY 12000024：提供者或UKey忙。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_GetUkeyPinAuthState(
@@ -156,25 +154,25 @@ struct OH_Huks_Result OH_Huks_GetUkeyPinAuthState(
     OH_Huks_ExternalPinAuthState *authState);
 
 /**
- * @brief 外部密钥管理能力扩展提供者获取属性信息。
+ * @brief 获取外部密钥管理能力扩展提供者的属性信息。
  *
  * @param resourceId 指定提供者的资源ID。
  * @param propertyId 指定按GMT 0016-2023定义的属性函数名称。
  * @param paramSetIn 指向输入操作参数的指针。
  * @param paramSetOut 指向输出参数的指针，且必须包含参数OH_HUKS_EXT_CRYPTO_TAG_EXTRA_DATA。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801 - 不支持的API。
- *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005 - IPC通信失败。
- *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006 - 驱动错误。
- *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011 - 未找到缓存的指定句柄。
- *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012 - 发生系统内部错误，处理函数未找到。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_ILLEGAL_ARGUMENT 12000018 - resourceId、propertyId、paramSet或回调无效。
- *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020 - 提供者或Ukey内部执行失败。
- *     <br>OH_HUKS_ERR_CODE_PIN_LOCKED 12000021 - PIN码被锁定。
- *     <br>OH_HUKS_ERR_CODE_PIN_NO_AUTH 12000023 - PIN码未通过认证。
- *     <br>OH_HUKS_ERR_CODE_BUSY 12000024 - 提供者或Ukey中的资源正在被使用。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_NOT_SUPPORTED_API 801：不支持的API。
+ *     <br>OH_HUKS_ERR_CODE_COMMUNICATION_FAIL 12000005：IPC通信失败。
+ *     <br>OH_HUKS_ERR_CODE_CRYPTO_FAIL 12000006：驱动错误。
+ *     <br>OH_HUKS_ERR_CODE_ITEM_NOT_EXIST 12000011：未找到缓存的指定句柄。
+ *     <br>OH_HUKS_ERR_CODE_INTERNAL_ERROR 12000012：发生系统内部错误，处理函数未找到。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：resourceId、propertyId、paramSetIn、paramSetOut存在无效参数。
+ *     <br>OH_HUKS_ERR_CODE_EXTERNAL_ERROR 12000020：提供者或UKey内部执行失败。
+ *     <br>OH_HUKS_ERR_CODE_PIN_LOCKED 12000021：PIN码被锁定。
+ *     <br>OH_HUKS_ERR_CODE_PIN_NO_AUTH 12000023：PIN码未通过认证。
+ *     <br>OH_HUKS_ERR_CODE_BUSY 12000024：提供者或UKey中的资源正在被使用。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_GetProperty(const struct OH_Huks_Blob *resourceId,
@@ -186,9 +184,9 @@ struct OH_Huks_Result OH_Huks_GetProperty(const struct OH_Huks_Blob *resourceId,
  *
  * @param paramSet 指向要初始化的参数集合的二级指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
- *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018 - params为NULL或paramSet无效。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：paramSet无效。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_InitExternalCryptoParamSet(OH_Huks_ExternalCryptoParamSet **paramSet);
@@ -200,8 +198,8 @@ struct OH_Huks_Result OH_Huks_InitExternalCryptoParamSet(OH_Huks_ExternalCryptoP
  * @param params 指向要添加的参数数组。
  * @param paramCnt 要添加的参数数量。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018 - params为NULL或paramSet无效。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：params为NULL或paramSet无效。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_AddExternalCryptoParams(OH_Huks_ExternalCryptoParamSet *paramSet,
@@ -212,9 +210,9 @@ struct OH_Huks_Result OH_Huks_AddExternalCryptoParams(OH_Huks_ExternalCryptoPara
  *
  * @param paramSet 指向要构建的参数集合的二级指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018 - paramSet无效。
- *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014 - 内存不足。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：paramSet无效。
+ *     <br>OH_HUKS_ERR_CODE_INSUFFICIENT_MEMORY 12000014：内存不足。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_BuildExternalCryptoParamSet(OH_Huks_ExternalCryptoParamSet **paramSet);
@@ -234,8 +232,8 @@ void OH_Huks_FreeExternalCryptoParamSet(OH_Huks_ExternalCryptoParamSet **paramSe
  * @param tag 指定要获取的参数标签值。
  * @param param 用于返回获取到的参数的二级指针。
  * @return 可能的返回码（errorCode）：
- *     <br>OH_HUKS_SUCCESS 0 - 操作成功。
- *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018 - paramSet或param无效，或参数在集合中不存在。
+ *     <br>OH_HUKS_SUCCESS 0：操作成功。
+ *     <br>OH_HUKS_ERR_CODE_INVALID_ARGUMENT 12000018：paramSet或param无效，或参数在集合中不存在。
  * @since 22
  */
 struct OH_Huks_Result OH_Huks_GetExternalCryptoParam(OH_Huks_ExternalCryptoParamSet *paramSet,
