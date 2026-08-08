7078b4d41473806a3984fcc804b3cfda036c999b
dreamyhhh <hangmengxin@huawei.com>
2026-08-08 13:25:40 +0800

    完善native_drawing头文件中文API注释(第2组: canvas/matrix/brush/path_effect/surface/point/record_cmd/round_rect/memory_stream/filter/mask_filter)
    Signed-off-by: dreamyhhh <hangmengxin@huawei.com>

--- zh-cn/graphic/graphic_2d/native_drawing/drawing_path_effect.h
+++ zh-cn/graphic/graphic_2d/native_drawing/drawing_path_effect.h
@@ -25,7 +25,12 @@
 /**
  * @file drawing_path_effect.h
  *
- * @brief This file declares the functions related to the path effect in the drawing module.
+ * @brief 文件中定义了与路径效果相关的功能函数。路径效果是对绘制路径进行几何变换的效果处理机制，
+ * 在路径绘制到画布之前对路径的几何形状进行修改，例如将尖角变为圆角、将连续路径变为虚线等。
+ * 多个路径效果可以通过组合（按顺序依次应用）
+ * 或叠加（各自独立应用后合并结果）的方式一起使用。支持创建组合路径效果、圆角路径效果、虚线路径效果、打散路径效果、
+ * 叠加路径效果等。
+ * <br>本模块为单线程模型策略，需要调用方自行管理线程安全和上下文状态的切换。
  *
  * @kit ArkGraphics2D
  * @library libnative_drawing.so
@@ -51,37 +56,40 @@ extern "C" {
  */
 typedef enum {
     /**
-     * 表示路径效果是平移效果。
+     * 表示虚线段沿路径平移绘制，不发生旋转或变形。
      */
     DRAWING_PATH_DASH_STYLE_TRANSLATE,
     /**
-     * 表示路径效果是旋转效果。
+     * 表示虚线段沿路径旋转，使其方向跟随路径切线方向。
      */
     DRAWING_PATH_DASH_STYLE_ROTATE,
     /**
-     * 表示路径效果是变形效果。
+     * 表示虚线段沿路径发生变形，以适应路径走向。
      */
     DRAWING_PATH_DASH_STYLE_MORPH,
 } OH_Drawing_PathDashStyle;
 
 /**
  * @brief 创建路径组合的路径效果对象。首先应用内部路径效果，然后应用外部路径效果。
+ * 使用完毕后必须调用{@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，否则会导致内存泄漏。
  *
  * @param outer 表示组合路径效果中外部路径效果{@link OH_Drawing_PathEffect}的指针。
  * @param inner 表示组合路径效果中内部路径效果{@link OH_Drawing_PathEffect}的指针。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
- * 如果返回nullptr，则创建失败，失败的原因可能是outer或者inner为nullptr。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ *     <br>如果返回nullptr，则创建失败，原因是outer或者inner为nullptr。
  * @since 18
  * @version 1.0
  */
 OH_Drawing_PathEffect* OH_Drawing_CreateComposePathEffect(OH_Drawing_PathEffect* outer, OH_Drawing_PathEffect* inner);
 
 /**
- * @brief 创建一个将路径的夹角变成指定半径的圆角的路径效果对象。
+ * @brief 创建一个将路径的夹角变成指定半径的圆角的路径效果对象。该路径效果会检测路径中的夹角（拐点），
+ * 并将尖角替换为指定半径的圆弧，使路径在拐点处平滑过渡。使用完毕后必须调用
+ * {@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，否则会导致内存泄漏。
  *
- * @param radius 表示圆角的半径，该值必须大于0时才生效。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
- * 如果返回nullptr，则创建失败，失败的可能原因是radius小于等于0。
+ * @param radius 表示圆角的半径，取值范围>0，单位为物理像素px。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ *     <br>如果返回nullptr，则创建失败，原因是radius小于等于0。
  * @since 18
  * @version 1.0
  */
@@ -89,38 +97,45 @@ OH_Drawing_PathEffect* OH_Drawing_CreateCornerPathEffect(float radius);
 
 /**
  * @brief 创建一个虚线效果的路径效果对象。虚线效果由一组虚线开的间隔、虚线关的间隔数据决定。
- * 本接口会产生错误码，可以通过{@link OH_Drawing_ErrorCodeGet}查看错误码的取值。
- * intervals为NULL或者count小于等于0时返回OH_DRAWING_ERROR_INVALID_PARAMETER。
+ * 使用完毕后必须调用{@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，否则会导致内存泄漏。
+ * <br>本接口会产生错误码，可以通过{@link OH_Drawing_ErrorCodeGet}查看错误码的取值。
+ * <br>intervals为nullptr或count小于等于0时返回OH_DRAWING_ERROR_INVALID_PARAMETER。
  *
- * @param intervals 虚线间隔数组首地址，偶数项的值表示虚线开的间隔长度，奇数项的值表示虚线关的间隔长度，单位为像素。
- * @param count 虚线间隔数组元素的个数，必须为大于0的偶数。
- * @param phase 虚线间隔数组中偏移量。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
+ * @param intervals 虚线间隔数组首地址，偶数项的值表示虚线可见段（开）的间隔长度，
+ * 奇数项的值表示虚线间隙段（关）的间隔长度，单位为物理像素px。
+ * @param count 虚线间隔数组元素的个数，取值范围>0，且为偶数。
+ * @param phase 虚线间隔数组中的偏移量，用于控制虚线绘制的起始位置，单位为物理像素px。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
  * @since 12
  * @version 1.0
  */
 OH_Drawing_PathEffect* OH_Drawing_CreateDashPathEffect(float* intervals, int count, float phase);
 
 /**
- * @brief 创建一种将路径打散并且在路径上产生不规则分布的路径效果对象。
+ * @brief 创建一种将路径打散并且在路径上产生不规则分布的路径效果对象。该路径效果按照segLength将路径分割为多个线段，
+ * 并对每个线段的末端点在deviation范围内进行随机偏移，从而产生不规则分布的视觉效果。
+ * 使用完毕后必须调用{@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，否则会导致内存泄漏。
  *
- * @param segLength 表示路径中每进行一次打散操作的长度，该值大于0时有效果。
- * @param deviation 表示绘制时的末端点的最大移动偏离量。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
+ * @param segLength 表示路径中每进行一次打散操作的长度，取值范围>0，单位为物理像素px。
+ * @param deviation 表示绘制时的末端点的最大移动偏离量，单位为物理像素px。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
  * @since 18
  * @version 1.0
  */
 OH_Drawing_PathEffect* OH_Drawing_CreateDiscretePathEffect(float segLength, float deviation);
 
 /**
- * @brief 创建一个虚线效果的路径效果对象。
+ * @brief 创建一个虚线效果的路径效果对象，使用指定路径作为虚线段样式，按照advance指定的步长沿目标路径重复排列。
+ * 与{@link OH_Drawing_CreateDashPathEffect}使用虚线间隔数组控制开关不同，
+ * 本接口使用指定路径作为虚线段形状。使用完毕后必须调用{@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，
+ * 否则会导致内存泄漏。
  *
  * @param path 表示虚线样式的路径对象{@link OH_Drawing_Path}的指针。
- * @param advance 表示虚线段的步长。
- * @param phase 表示虚线段内图形在虚线步长范围内的偏移量。
- * @param type 表示虚线路径效果样式。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
- * 如果返回nullptr，则创建失败，失败的可能原因是path为nullptr或者advance小于等于0。
+ * @param advance 表示虚线段的步长，取值范围>0，单位为物理像素px。
+ * @param phase 表示虚线样式的起始偏移量，用于控制虚线段绘制的起始位置，单位为物理像素px。
+ * @param type 表示虚线路径效果样式，取值见{@link OH_Drawing_PathDashStyle}枚举。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ *     <br>如果返回nullptr，则创建失败，原因是path为nullptr或者advance小于等于0。
  * @since 18
  * @version 1.0
  */
@@ -128,12 +143,14 @@ OH_Drawing_PathEffect* OH_Drawing_CreatePathDashEffect(const OH_Drawing_Path* pa
     OH_Drawing_PathDashStyle type);
 
 /**
- * @brief 创建一个使用两种路径效果分别生效后叠加的路径效果对象。
+ * @brief 创建一个使用两种路径效果叠加的路径效果对象。与{@link OH_Drawing_CreateComposePathEffect}的先后顺序应用不同，
+ * 本接口将两种路径效果各自独立应用后将结果叠加。使用完毕后必须调用
+ * {@link OH_Drawing_PathEffectDestroy}销毁该路径效果对象，否则会导致内存泄漏。
  *
- * @param firstPathEffect 指向路径对象{@link OH_Drawing_PathEffect}的指针。
- * @param secondPathEffect 指向路径对象{@link OH_Drawing_PathEffect}的指针。
- * @return 函数返回一个指针，指针指向创建的路径效果对象{@link OH_Drawing_PathEffect}。
- * 如果返回nullptr，则创建失败，失败的可能原因是firstPathEffect或者secondPathEffect为nullptr。
+ * @param firstPathEffect 表示参与叠加的第一个路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ * @param secondPathEffect 表示参与叠加的第二个路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ * @return 返回指向创建的路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ *     <br>如果返回nullptr，则创建失败，原因是firstPathEffect或者secondPathEffect为nullptr。
  * @since 18
  * @version 1.0
  */
@@ -141,9 +158,9 @@ OH_Drawing_PathEffect* OH_Drawing_CreateSumPathEffect(OH_Drawing_PathEffect* fir
     OH_Drawing_PathEffect* secondPathEffect);
 
 /**
- * @brief 销毁路径效果对象并回收该对象占有内存。
+ * @brief 销毁路径效果对象，并回收该对象占用的内存。路径效果对象使用完毕后必须调用此方法，否则会导致内存泄漏。
  *
- * @param pathEffect 指向路径效果对象{@link OH_Drawing_PathEffect}的指针。
+ * @param pathEffect 指向需要销毁的路径效果对象{@link OH_Drawing_PathEffect}的指针。
  * @since 12
  * @version 1.0
  */
