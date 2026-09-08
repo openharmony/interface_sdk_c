513ed9d32f91e2481828c3f625611d079646c7fb
wangyongli66 <wangyongli7@h-partners.com>
2026-09-08 21:01:53 +0800

    update: 更新文件 avtranscoder_base.h
    Signed-off-by: wangyongli66 <wangyongli7@h-partners.com>

--- zh-cn/multimedia/player_framework/avtranscoder_base.h
+++ zh-cn/multimedia/player_framework/avtranscoder_base.h
@@ -17,7 +17,7 @@
  * @{
  *
  * @brief 提供转码的请求能力接口。
- *
+ * 根据实际的开发需求，参考对应的开发指南及样例[使用AVTranscoder实现音视频转码](docroot://media/media/using-ndk-avtranscoder-for-transcodering.md)。
  * @syscap SystemCapability.Multimedia.Media.AVTranscoder
  * @since 20
  * @}
