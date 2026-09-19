6cc911d5c2e330e7a96f62d596d8892e3e36a942
yangwei <yangwei271@huawei.com>
2026-09-19 16:16:54 +0800

    pc占位符
    Signed-off-by: yangwei <yangwei271@huawei.com>

--- zh-cn/filemanagement/cloud_disk_manager/include/oh_cloud_disk_manager.h
+++ zh-cn/filemanagement/cloud_disk_manager/include/oh_cloud_disk_manager.h
@@ -456,14 +456,14 @@ typedef struct CloudDisk_SyncFolder {
 
 /**
  * @brief 定义带占位符支持的云盘同步文件夹。
- * 必须将版本字段设置为有效的版本宏(例如{@link Cloud_DISK_SYNC_LAYER_EX_VERSION_1})，然后才能传递结构到任何API。
+ * 必须将版本字段设置为有效的版本宏(例如{@link OH_CLOUD_DISK_SYNC_FOLDER_EX_VERSION_1})，然后才能传递结构到任何API。
  * 运行时使用版本来确定字段有效；当指定低版本时，在较高版本中引入的字段将被忽略。
  *
  * @since 26.1.0
  */
 typedef struct OH_CloudDisk_SyncFolderEx {
     /**
-     * @brief 指示此结构体的版本。必须初始化为有效的版本宏，例如 {@link Cloud_DISK_SYNC_FLYER_EX_VERSION_1}。
+     * @brief 指示此结构体的版本。必须初始化为有效的版本宏，例如 {@link OH_CLOUD_DISK_SYNC_FOLDER_EX_VERSION_1}。
      *
      * @since 26.1.0
      */
